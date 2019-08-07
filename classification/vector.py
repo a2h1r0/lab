3ffbc80da1174231f81837e6d90a3a3577d262cf
a2h1r0 <atsu3.n9@gmail.com>
2019-08-07 16:43:39 +0900

    New

--- classification/vector.py
+++ classification/vector.py
@@ -1,18 +1,37 @@
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 
+data_size = 500
+tester_size = 9
+true_color = 'red'
+
 # CSVファイルの読み込み
-attack = np.loadtxt('exam_attack.csv', delimiter = ",", dtype = float, skiprows = 1, usecols = range(0,20))
-train = np.loadtxt('exam_train.csv', delimiter = ",", dtype = float, skiprows = 1, usecols = range(0,20))
-i = 0
-height = np.empty(len(attack)*len(train))
+attack = np.loadtxt('vector_attack.csv', delimiter = ",", dtype = float, skiprows = 1, usecols = range(0,20))
+train = np.loadtxt('vector_train.csv', delimiter = ",", dtype = float, skiprows = 1, usecols = range(0,20))
+
+# 計算結果数の配列(1人あたりのデータ数×学習データ数)を被験者数分作成
+distance = [np.empty(data_size*len(train)) for j in range(tester_size)]
+
+i=0
+line=-1
 
 for vec in attack:
+    if (i%(data_size*len(train)) == 0):
+        line+=1
+        i=0
     for vec2 in train:
-        height[i] = np.linalg.norm(vec-vec2)
+        distance[line][i] = np.linalg.norm(vec-vec2)
         i+=1
 
-#left = list(range(i))
+x = list(range(data_size*len(train)))
 
+print("正解データは%sで描画" % true_color)
+plt.plot(x, distance[0], true_color)
+for j in range(1, 9):
+    r = lambda: random.randint(0,255)
+    code = "#{:02X}{:02X}{:02X}"
+    color = code.format(r(),r(),r())
+    plt.plot(x, distance[j], color)
 
-plt.bar(left, height, width=1.0)
+plt.show()
