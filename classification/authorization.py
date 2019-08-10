d9e9a0c24aadef7d5550e504c7c44863c7fa219e
a2h1r0 <atsu3.n9@gmail.com>
2019-08-10 14:45:08 +0900

    Update authorization.py

--- classification/authorization.py
+++ classification/authorization.py
@@ -15,46 +15,59 @@ attack_file = np.loadtxt('vector_attack.csv', delimiter = ",", dtype = float,
 train_file = np.loadtxt('vector_train.csv', delimiter = ",", dtype = float, 
                    skiprows = 1, usecols = range(0,20))  # 学習データ
 
-# x軸の配列を被験者1人あたりの計算結果数(1人あたりのデータ数×学習データ数)分作成
-x = list(range(data_size*len(train_file)))
-# 計算結果の配列を被験者数分作成
-distance = [np.empty(data_size*len(train_file)) for j in range(tester_size)]
+x = np.arange(min, max, 0.1)
 
 # カウンタ用変数の初期化
-tester = num = i = 0
-min = min*10
-max = max*10
+tester = loop = threshold = 0
+min = int(min*10)
+max = int(max*10)
 
-for threshold in range(min, max):
+TAN = np.empty(max-min) # True Acceptance Number(本人受入数)
+FAN = np.empty(max-min) # False Acceptance Number(他人受入数)
+TRN = np.empty(max-min) # True Rejection Number(他人拒否数)
+FRN = np.empty(max-min) # False Rejection Number(本人拒否数)
+
+
+for thresholds in range(min, max):
     for attack in attack_file:
         for train in train_file:
-            dis = np.linalg.norm(attack-train)
-            if (dis <= (threshold/10)):
-                true += 1
-            else:
-                false += 1
-                
-            num += 1
-
-        if (num == (data_size*len(train_file))):
-            tester += 1
+            distance = np.linalg.norm(attack-train)
             
-        if ((num/(data_size*len(train_file))) == 1):
-            tester += 1
-                
-        trues[tester][i] = true
-        falses[tester][i] = false
-    i += 1  # 次の閾値に
-    tester = 0
+            if (distance <= (thresholds/10)):
+                if (loop < (data_size*len(train_file))):
+                    TAN[threshold] += 1
+                else:
+                    FAN[threshold] += 1
+                    
+            elif (distance > (thresholds/10)):
+                if (loop < (data_size*len(train_file))):
+                    FRN[threshold] += 1
+                else:
+                    TRN[threshold] += 1
+            loop += 1
             
+    threshold += 1  # 次の閾値に
+    loop = 0
+            
+for i in range(threshold):
+    TAN[i] = TAN[i]/(data_size*len(train_file))
+    FAN[i] = TAN[i]/(data_size*len(train_file)*(tester_size-1))
+    TRN[i] = TAN[i]/(data_size*len(train_file)*(tester_size-1))
+    FRN[i] = TAN[i]/(data_size*len(train_file))
 
-# 計算結果を描画
-print("正解データは%sで描画" % true_color)
-plt.ylabel("distance")
-plt.plot(x, distance[0], true_color)      # 1人目(正解)のデータをプロット
-for tester in range(1, tester_size):      # 2人目以降のデータをプロット
-    r = lambda: random.randint(0, 255)    # グラフの色をランダムに生成
-    code = "#{:02X}{:02X}{:02X}"
-    color = code.format(r(),r(),r())
-    plt.plot(x, distance[tester], color)
+    # 計算結果を描画
+#plt.xlim(min/10, max/10)
+#plt.ylim(0, (data_size*len(train_file)))
+plt.xlabel("threshold")
+plt.plot(x, TAN, 'r', label="True Acceptance Number")      # 1人目(正解)のデータをプロット
+plt.plot(x, FAN, 'g', label="False Acceptance Number")
+plt.plot(x, FRN, 'b', label="True Rejection Number")
+plt.plot(x, TRN, 'y', label="False Rejection Number")
+plt.legend()
+
+#for tester in range(1, tester_size):      # 2人目以降のデータをプロット
+#    r = lambda: random.randint(0, 255)    # グラフの色をランダムに生成
+#    code = "#{:02X}{:02X}{:02X}"
+#    color = code.format(r(),r(),r())
+#    plt.plot(x, distance[tester], color)
 plt.show()
