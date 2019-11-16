469a0b8387725a0c132a975c7023f13858c53445
a2h1r0 <atsu3.n9@gmail.com>
2019-11-16 15:31:02 +0900

    update

--- get_csv_ontime/get_csv_ontime.py
+++ get_csv_ontime/get_csv_ontime.py
@@ -3,12 +3,14 @@ from time import sleep
 import os.path
 import csv
 import math
+import datetime
 
 ser = serial.Serial('COM5', 9600)   # Arduino1号機
 ser2 = serial.Serial('COM6', 9600)  # Arduino2号機
 filename = 'attack.csv'               # データ保存先ファイル
-time = 3                          # データ取得時間(秒単位)
+time = 5                          # データ取得時間(秒単位)
 tester = "fujii"                     # 正解ラベル(被験者名)
+
 
 sleep(1)    # ポート準備に1秒待機**これがないとシリアル通信がうまく動かない**
 
@@ -20,14 +22,15 @@ with open(filename, 'a', newline='') as f:
     writer = csv.writer(f)
 
     if exist == 0:      # ファイルが新規作成の場合，ラベルを付与する
-        writer.writerow(["in0","in1","in2","in3","in4","in5","in6","in7","in8",
-                         "in9","inA","inB","inC","inあ","inい","inう",
-                         "in10","in11","in12","in13","in14","in15","in16",
-                         "in17","in18","in19","inD","inE","inF","inア","inイ",
-                         "inウ","Time","Tester"])
+        writer.writerow(["in0","in1","in2","in3","in4","in5","in6","in7","in8","in9",
+                         "inあ","inい","inう","inA","inB","inC",
+                         "in10","in11","in12","in13","in14","in15","in16","in17","in18","in19",
+                         "inア","inイ","inウ","inD","inE","inF",
+                         "Time","Tester","Date"])
     elif exist == 1:    # ファイルが既存の場合，ラベルを付与しない
         print("File exist. Didn't give label.")
-   
+        
+    date = 0
     # データの取得と書き込み
     while True:
         # シリアル通信
@@ -45,6 +48,9 @@ with open(filename, 'a', newline='') as f:
 
         voltage.extend(voltage2)    # 1号機と2号機のデータを結合
         voltage.append(tester)      # 最終列に正解ラベル(被験者名)を追加
+        if (date == 0):
+            voltage.append(datetime.date.today())
+            date += 1
         
         writer.writerow(voltage)    # ファイルへ書き込み
 
