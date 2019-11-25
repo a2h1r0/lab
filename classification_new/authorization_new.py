0eb4b26ea294a860950c9df06372e6bcca5666ec
a2h1r0 <atsu3.n9@gmail.com>
2019-11-25 12:35:30 +0900

    update

--- classification_new/authorization_new.py
+++ classification_new/authorization_new.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import itertools
+from statistics import mean
 
 MIN = 1.5           # 閾値の下限
 MAX = 3.0           # 閾値の上限
@@ -19,54 +20,74 @@ for i in range(len(tester)):
 
 norm_ave = [[] for i in range(len(tester))]
 for order in range(len(tester)):
-    train_num = 0   # データ変更時に初期化   
+    num = 0   # データ変更時に初期化   
     norm_sum = 0
 
     for row in data[order].itertuples(name=None): # 1行ずつ読み出し
         if not (row[-1] in [0,1]):    # 最初以外の区切り文字が出てきたら実行，出てこない間はスキップ
-            norm_ave[order].append(norm_sum/train_num)    # 区切りごとに平均を保存
-            print(train_num)
-            train_num = 0  # カウンタを初期化
+            norm_ave[order].append(norm_sum/num)    # 区切りごとに平均を保存
+            num = 0  # カウンタを初期化
             norm_sum = 0    # ベクトル合計を初期化
             
                  
         norm = np.linalg.norm(row[1:34]) # ベクトル距離計算
         norm_sum += norm    # 距離の合計に加算
-        train_num += 1 # 計算回数を増加
+        num += 1 # 計算回数を増加(データ数の確認)
         
-    norm_ave[order].append(norm_sum/train_num)   # 最終要素には区切り文字がないため．
+    norm_ave[order].append(norm_sum/num)   # 最終要素には区切り文字がないため．
 
 
+FRR = np.zeros(len(norm_ave)) # False Rejection Rate(本人拒否率)
+FAR = np.zeros(len(norm_ave)) # False Acceptance Rate(他人受入率)
 
-for order in range(len(tester)):    # 1人ごと学習データにする．
-    print(tester[order] + "が学習データです．")
+for train in range(len(tester)):    # 1人ごと学習データにする．
+    print(tester[train] + "が学習データです．")
 
-    conbinations = list(itertools.combinations(range(len(norm_ave[order])), train_size))    # 組み合わせ取得
-    
-    for conb_order in range(len(conbinations)):     # 組み合わせの個数だけ比較
-        print("組み合わせは ")
-        print(np.array(conbinations[conb_order])+np.array([1,1]))
-        print(" です．")
+    combinations = list(itertools.combinations(range(len(norm_ave[train])), train_size))    # 組み合わせ取得
+    FRR_temp = np.zeros(len(combinations))
+    FAR_temp = np.zeros(len(combinations))
+    for order in range(len(combinations)):     # 組み合わせの個数だけ比較，1人ごとに交差検証
+        num = 0
 
-        train_data = []        
-        for i in range(train_size):
-            train_data.append(norm_ave[order][conbinations[conb_order][i]])
-            
         ##まずは自分と計算
-        threshold = 0.0000001
-        distance_small = float('inf')
-        for i in range(train_size): # 学習データ数1つずつ比較
-            for j in range(len(norm_ave[order])):   # 1人のデータ数回比較していく
-                if (j in conbinations[conb_order]): # 学習データに用いているデータ同士は比較しない
+        threshold = 0.1
+        for attack in range(len(tester)):  # 1人ごとに認証データにする
+            
+            for i in range(len(norm_ave[train])):   # 1人のデータ数回比較していく，1データずつ処理
+                if (attack == train and i in combinations[order]): # 比較しているのが自分かつ，学習データに用いているデータ同士は比較しない
                     continue
-                distance = abs(train_data[i]-norm_ave[order][j])    # 距離の絶対値
+                distance_small = float('inf')
+                for j in range(train_size): # 学習データ数1つずつ比較
+                    distance = abs(norm_ave[train][i]-norm_ave[train][combinations[order][j]])    # 距離の絶対値
+                    if (distance < distance_small):
+                        distance_small = distance
+                    ## ここで組み合わせの中で距離が小さい方を保存
+            
+              
+                # 距離の差が閾値以下なら，受け入れる
+                if (distance_small <= threshold and attack != train):
+                    FAR_temp[order] += 1 # 他人受入数
+                # 距離が閾値より大きいなら，弾く
+                elif (distance_small > threshold and attack == train):
+                    FRR_temp[order] += 1 # 本人拒否数
+                    
+                num += 1    # 判別回数を増加
                 
-                if (distance < distance_small):
-                    distance_small = distance
-
-                if (distance_small <= threshold):
-                    ok[] += 1
-                else:
-                    print("ng")
-                        
-        ##他人と比較
+                    
+                    
+                
+        ## 割合計算，組み合わせが変わるごとに計算
+        num = num-(len(norm_ave[train])-train_size) # 判別回数から自分と比較した回数を引く，総データ数-学習に使う数が自分の判別回数
+        FRR_temp[order] = FRR_temp[order]/(len(norm_ave[train])-train_size)
+        FAR_temp[order] = FAR_temp[order]/num
+        
+    FRR[train] = mean(FRR_temp)*100
+    FAR[train] = mean(FAR_temp)*100
+        
+    # 1人ごとに結果を表示，交差検証の平均
+    print("FRR:", end="")
+    print(FRR[train]/len(norm_ave[train]))        
+    print("FAR:", end="")
+    print(FAR[train]/len(norm_ave[train]))
+        
+        
--- classification_new/authorization_new_bk.py
+++ classification_new/authorization_new_bk.py
@@ -7,49 +7,67 @@ MAX = 3.0           # 閾値の上限
 
 tester = ["fujii", "ooyama", "matsuda", "kajiwara"]
 data = [[0], [0], [0], [0]]
-separator = [[0], [0], [0], [0]]
+train_size = 2 # 学習する個数
 
-for read in range(len(tester)):
-    filename = tester[read] + ".csv"
-    data[read] = np.loadtxt(filename, delimiter = ",", dtype = float, skiprows = 1, usecols = range(0,32))   # 認証データ
-    separator[read] = pd.read_csv(filename, usecols=['Number'], encoding='Shift-JIS')
-    separator[read].fillna(0, inplace=True)
+for i in range(len(tester)):
+    filename = tester[i] + ".csv"
+    data[i] = pd.read_csv(filename, usecols=["in0","in1","in2","in3","in4","in5","in6","in7","in8","in9",
+                         "inあ","inい","inう","inA","inB","inC",
+                         "in10","in11","in12","in13","in14","in15","in16","in17","in18","in19",
+                         "inア","inイ","inウ","inD","inE","inF","Time","Number"], encoding='Shift-JIS')
+    data[i].fillna(0, inplace=True)
 
-
+norm_ave = [[] for i in range(len(tester))]
 for order in range(len(tester)):
+    train_num = 0   # データ変更時に初期化   
+    norm_sum = 0
+
+    for row in data[order].itertuples(name=None): # 1行ずつ読み出し
+        if not (row[-1] in [0,1]):    # 最初以外の区切り文字が出てきたら実行，出てこない間はスキップ
+            norm_ave[order].append(norm_sum/train_num)    # 区切りごとに平均を保存
+            print(train_num)
+            train_num = 0  # カウンタを初期化
+            norm_sum = 0    # ベクトル合計を初期化
+            
+                 
+        norm = np.linalg.norm(row[1:34]) # ベクトル距離計算
+        norm_sum += norm    # 距離の合計に加算
+        train_num += 1 # 計算回数を増加
+        
+    norm_ave[order].append(norm_sum/train_num)   # 最終要素には区切り文字がないため．
+
+
+
+for order in range(len(tester)):    # 1人ごと学習データにする．
     print(tester[order] + "が学習データです．")
 
-    conbinations = list(itertools.combinations(range(1, int(separator[order].max()+1)), 2))
-
-    for i in range(len(conbinations)):
+    conbinations = list(itertools.combinations(range(len(norm_ave[order])), train_size))    # 組み合わせ取得
+    
+    for conb_order in range(len(conbinations)):     # 組み合わせの個数だけ比較
         print("組み合わせは ")
-        print(conbinations[i])
+        print(np.array(conbinations[conb_order])+np.array([1,1]))
         print(" です．")
-        
-        norm_sum = 0
-        number = -1
-        train_size = 0
-        train_start = 0
-        train_order = -1
-        norm_ave = [0, 0]
-        for train in data[order]:
-            number += 1
-            if (int(separator[order].iloc[number]) != zero):
-                if (separator[order].iloc[number] in conbinations[i]):
-                    train_start = 1
-                    train_order += 1
+
+        train_data = []        
+        for i in range(train_size):
+            train_data.append(norm_ave[order][conbinations[conb_order][i]])
+            
+        ##まずは自分と計算
+        threshold = 0.0000001
+        distance_small = float('inf')
+        for i in range(train_size): # 学習データ数1つずつ比較
+            for j in range(len(norm_ave[order])):   # 1人のデータ数回比較していく
+                if (j in conbinations[conb_order]): # 学習データに用いているデータ同士は比較しない
+                    continue
+                distance = abs(train_data[i]-norm_ave[order][j])    # 距離の絶対値
+                print(distance)
+                
+                if (distance < distance_small):
+                    distance_small = distance
+
+                if (distance_small <= threshold):
+                    print("ok")
                 else:
-                    if (train_size != 0):
-                        norm_ave[train_order] = norm_sum / train_size
-                        train_size = 0
-                        train_start = 0
-        
-            if (train_start == 1):
-                norm = np.linalg.norm(train) # ベクトル距離計算
-                norm_sum += norm
-                train_size += 1
-          
-        if (train_start != 0):  # ファイル終了時は区切り文字が出てこないため
-            norm_ave[train_order] = norm_sum / train_size
+                    print("ng")
                         
-print(norm_ave)
+        ##他人と比較
