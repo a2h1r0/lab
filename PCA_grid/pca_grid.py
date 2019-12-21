e3077749184ab6d00a8d1058d6537c835fe87695
a2h1r0 <atsu3.n9@gmail.com>
2019-12-21 14:01:57 +0900

    update

---
+++ PCA_grid/pca_grid.py
@@ -0,0 +1,36 @@
+tester = ["ooyama", "okamoto", "kajiwara", "sawano", "nagamatsu", "noda", "hatta", "matsuda"]  # **被験者**
+subject = ["A", "B", "C", "D", "E", "F", "G", "H"]  # **プロット名**
+marker = ["o", ",", "^", "*", "o", ",", "^", "*"]   # **プロットマーカー**
+#################
+
+
+
+import calculate_vector_ave as cal
+from sklearn import decomposition
+import matplotlib.pyplot as plt
+
+# ベクトルの平均値を計算
+vector_ave = cal.calculate_vector_ave(tester)
+
+# testにベクトルの平均値をまとめる
+test = []
+for name in range(len(tester)):
+    test.extend(vector_ave[name])
+
+# 計算
+model = decomposition.PCA(n_components=2)
+model.fit(test)
+compressed = model.transform(test)
+
+# 描画
+plt.xlabel("First component", fontsize=14)
+plt.ylabel("Second component", fontsize=14)
+plt.tick_params(labelsize=14)
+long = 0
+for num, item, name in zip(range(len(tester)), marker, subject):
+    plt.scatter(compressed[long:long+len(vector_ave[num]), 0],
+                compressed[long:long+len(vector_ave[num]), 1], marker=item, label="Subject "+name)
+    long += len(vector_ave[num])
+plt.legend()
+plt.show()
+#plt.savefig("PCA.svg")
