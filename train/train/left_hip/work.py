b1c8f1450d7b610c0fdb79471e61e5717da77054
a2h1r0 <atsu3.n9@gmail.com>
2020-02-13 22:35:59 +0900

    Create work.py

---
+++ train/train/left_hip/work.py
@@ -0,0 +1,546 @@
+"""
+### subject1 ###
+
+## sandwich ##
+# Take
+subject1_file_23,sandwich,Take,
+subject1_file_85,sandwich,Take,
+subject1_file_119,sandwich,Take,
+subject1_file_202,sandwich,Take,
+subject1_file_304,sandwich,Take,
+subject1_file_557,sandwich,Take,
+subject1_file_754,sandwich,Take,
+subject1_file_831,sandwich,Take,
+subject1_file_907,sandwich,Take,
+subject1_file_910,sandwich,Take,
+subject1_file_973,sandwich,Take,
+
+# Take,Wash
+subject1_file_491,sandwich,Take,Wash,
+subject1_file_523,sandwich,Take,Wash,
+
+# Cut,Wash
+subject1_file_777,sandwich,Cut,Wash,
+
+# Take,Cut,Wash
+subject1_file_244,sandwich,Take,Cut,Wash,
+subject1_file_300,sandwich,Take,Cut,Wash,
+subject1_file_429,sandwich,Take,Cut,Wash,
+subject1_file_526,sandwich,Take,Cut,Wash,
+
+# Cut,other
+subject1_file_131,sandwich,Cut,other,
+subject1_file_160,sandwich,Cut,other,
+subject1_file_596,sandwich,Cut,other,
+subject1_file_681,sandwich,Cut,other,
+subject1_file_928,sandwich,Cut,other,
+
+# Put,other
+subject1_file_248,sandwich,Put,other,
+subject1_file_265,sandwich,Put,other,
+subject1_file_286,sandwich,Put,other,
+subject1_file_485,sandwich,Put,other,
+subject1_file_575,sandwich,Put,other,
+subject1_file_608,sandwich,Put,other,
+subject1_file_660,sandwich,Put,other,
+subject1_file_796,sandwich,Put,other,
+subject1_file_842,sandwich,Put,other,
+subject1_file_845,sandwich,Put,other,
+subject1_file_859,sandwich,Put,other,
+subject1_file_864,sandwich,Put,other,
+
+# other,Cut,Wash
+subject1_file_948,sandwich,other,Cut,Wash,
+
+# other
+subject1_file_172,sandwich,other,
+subject1_file_322,sandwich,other,
+
+# other,other
+subject1_file_9,sandwich,other,other,
+
+
+## cereal ##
+# Take
+subject1_file_113,cereal,Take,
+subject1_file_208,cereal,Take,
+subject1_file_319,cereal,Take,
+subject1_file_479,cereal,Take,
+subject1_file_531,cereal,Take,
+subject1_file_567,cereal,Take,
+subject1_file_712,cereal,Take,
+subject1_file_868,cereal,Take,
+subject1_file_922,cereal,Take,
+subject1_file_964,cereal,Take,
+
+# Take,Peel
+subject1_file_60,cereal,Take,Peel,
+subject1_file_559,cereal,Take,Peel,
+
+# Take,Cut,Peel
+subject1_file_779,cereal,Take,Cut,Peel,
+
+# Open,Cut,Peel
+subject1_file_372,cereal,Open,Cut,Peel,
+subject1_file_516,cereal,Open,Cut,Peel,
+
+# Take,Open,Cut,Peel
+subject1_file_116,cereal,Take,Open,Cut,Peel,
+subject1_file_499,cereal,Take,Open,Cut,Peel,
+subject1_file_763,cereal,Take,Open,Cut,Peel,
+
+# Pour,Put,Open,other
+subject1_file_47,cereal,Pour,Put,Open,other,
+subject1_file_541,cereal,Pour,Put,Open,other,
+subject1_file_553,cereal,Pour,Put,Open,other,
+subject1_file_741,cereal,Pour,Put,Open,other,
+subject1_file_855,cereal,Pour,Put,Open,other,
+
+# Pour,Open,Cut,Put,other
+subject1_file_876,cereal,Pour,Open,Cut,Put,other,
+
+# other
+subject1_file_24,cereal,other,
+
+
+## fruitsalad ##
+# Take
+subject1_file_80,fruitsalad,Take,
+subject1_file_877,fruitsalad,Take,
+
+# Peel
+subject1_file_552,fruitsalad,Peel,
+subject1_file_661,fruitsalad,Peel,
+
+# Take,Peel
+subject1_file_515,fruitsalad,Take,Peel,
+subject1_file_939,fruitsalad,Take,Peel,
+
+# Take,Put,Peel
+subject1_file_416,fruitsalad,Take,Put,Peel,
+subject1_file_613,fruitsalad,Take,Put,Peel,
+
+# Take,Add,Mix
+subject1_file_111,fruitsalad,Take,Add,Mix,
+subject1_file_975,fruitsalad,Take,Add,Mix,
+
+# Put,Cut,Peel
+subject1_file_58,fruitsalad,Put,Cut,Peel,
+subject1_file_264,fruitsalad,Put,Cut,Peel,
+subject1_file_303,fruitsalad,Put,Cut,Peel,
+subject1_file_947,fruitsalad,Put,Cut,Peel,
+
+# other,Mix
+subject1_file_771,fruitsalad,other,Mix,
+subject1_file_919,fruitsalad,other,Mix,
+
+
+
+### subject2 ###
+
+## sandwich ##
+# Take
+subject2_file_165,sandwich,Take,
+subject2_file_192,sandwich,Take,
+subject2_file_305,sandwich,Take,
+subject2_file_809,sandwich,Take,
+subject2_file_983,sandwich,Take,
+subject2_file_985,sandwich,Take,
+
+# Put
+subject2_file_47,sandwich,Put,
+subject2_file_74,sandwich,Put,
+subject2_file_173,sandwich,Put,
+subject2_file_490,sandwich,Put,
+subject2_file_506,sandwich,Put,
+subject2_file_677,sandwich,Put,
+subject2_file_954,sandwich,Put,
+
+# Take,Wash
+subject2_file_80,sandwich,Take,Wash,
+subject2_file_519,sandwich,Take,Wash,
+subject2_file_615,sandwich,Take,Wash,
+subject2_file_650,sandwich,Take,Wash,
+subject2_file_918,sandwich,Take,Wash,
+subject2_file_955,sandwich,Take,Wash,
+
+# Cut,Wash
+subject2_file_136,sandwich,Cut,Wash,
+subject2_file_362,sandwich,Cut,Wash,
+subject2_file_687,sandwich,Cut,Wash,
+subject2_file_701,sandwich,Cut,Wash,
+subject2_file_703,sandwich,Cut,Wash,
+subject2_file_965,sandwich,Cut,Wash,
+
+# Put,other
+subject2_file_24,sandwich,Put,other,
+subject2_file_116,sandwich,Put,other,
+subject2_file_361,sandwich,Put,other,
+subject2_file_365,sandwich,Put,other,
+subject2_file_604,sandwich,Put,other,
+subject2_file_696,sandwich,Put,other,
+subject2_file_776,sandwich,Put,other,
+
+# Cut,other
+subject2_file_559,sandwich,Cut,other,
+
+# Put,Cut,other
+subject2_file_82,sandwich,Put,Cut,other,
+subject2_file_253,sandwich,Put,Cut,other,
+subject2_file_633,sandwich,Put,Cut,other,
+subject2_file_812,sandwich,Put,Cut,other,
+subject2_file_911,sandwich,Put,Cut,other,
+
+
+## cereal ##
+# Take
+subject2_file_308,cereal,Take,
+subject2_file_496,cereal,Take,
+subject2_file_534,cereal,Take,
+subject2_file_550,cereal,Take,
+subject2_file_608,cereal,Take,
+subject2_file_761,cereal,Take,
+
+# Take,Peel
+subject2_file_195,cereal,Take,Peel,
+subject2_file_528,cereal,Take,Peel,
+
+# Take,Cut,Peel
+subject2_file_38,cereal,Take,Cut,Peel,
+subject2_file_85,cereal,Take,Cut,Peel,
+subject2_file_370,cereal,Take,Cut,Peel,
+subject2_file_996,cereal,Take,Cut,Peel,
+
+# Pour,Open,Cut,Peel,Put
+subject2_file_478,cereal,Pour,Open,Cut,Peel,Put,
+subject2_file_856,cereal,Pour,Open,Cut,Peel,Put,
+
+# other
+subject2_file_142,cereal,other,
+subject2_file_358,cereal,other,
+subject2_file_781,cereal,other,
+
+# Put,other
+subject2_file_196,cereal,Put,other,
+
+# Pour,Put,other
+subject2_file_499,cereal,Pour,Put,other,
+
+# Pour,Open,Cut,Put,other
+subject2_file_184,cereal,Pour,Open,Cut,Put,other,
+subject2_file_486,cereal,Pour,Open,Cut,Put,other,
+subject2_file_629,cereal,Pour,Open,Cut,Put,other,
+subject2_file_668,cereal,Pour,Open,Cut,Put,other,
+
+
+## fruitsalad ##
+# Take
+subject2_file_556,fruitsalad,Take,
+subject2_file_590,fruitsalad,Take,
+subject2_file_654,fruitsalad,Take,
+subject2_file_824,fruitsalad,Take,
+subject2_file_859,fruitsalad,Take,
+subject2_file_912,fruitsalad,Take,
+
+# Peel
+subject2_file_87,fruitsalad,Peel,
+subject2_file_182,fruitsalad,Peel,
+subject2_file_522,fruitsalad,Peel,
+subject2_file_782,fruitsalad,Peel,
+subject2_file_835,fruitsalad,Peel,
+
+# Take,Peel
+subject2_file_212,fruitsalad,Take,Peel,
+subject2_file_364,fruitsalad,Take,Peel,
+subject2_file_412,fruitsalad,Take,Peel,
+subject2_file_415,fruitsalad,Take,Peel,
+subject2_file_472,fruitsalad,Take,Peel,
+subject2_file_474,fruitsalad,Take,Peel,
+
+# Put,Peel
+subject2_file_231,fruitsalad,Put,Peel,
+subject2_file_908,fruitsalad,Put,Peel,
+
+# Cut,Peel
+subject2_file_193,fruitsalad,Cut,Peel,
+subject2_file_641,fruitsalad,Cut,Peel,
+subject2_file_951,fruitsalad,Cut,Peel,
+
+# Put,Cut,Peel
+subject2_file_95,fruitsalad,Put,Cut,Peel,
+subject2_file_314,fruitsalad,Put,Cut,Peel,
+subject2_file_350,fruitsalad,Put,Cut,Peel,
+subject2_file_372,fruitsalad,Put,Cut,Peel,
+subject2_file_413,fruitsalad,Put,Cut,Peel,
+subject2_file_455,fruitsalad,Put,Cut,Peel,
+subject2_file_533,fruitsalad,Put,Cut,Peel,
+subject2_file_756,fruitsalad,Put,Cut,Peel,
+subject2_file_938,fruitsalad,Put,Cut,Peel,
+subject2_file_941,fruitsalad,Put,Cut,Peel,
+
+# Take,Put,Cut,Peel
+subject2_file_185,fruitsalad,Take,Put,Cut,Peel,
+subject2_file_610,fruitsalad,Take,Put,Cut,Peel,
+
+# Take,Put,Cut,Add
+subject2_file_78,fruitsalad,Take,Put,Cut,Add,
+
+# Take,Cut,Peel,Put,Add
+subject2_file_877,fruitsalad,Take,Cut,Peel,Put,Add,
+
+# Take,Cut,Put,Add,Mix
+subject2_file_661,fruitsalad,Take,Cut,Put,Add,Mix,
+
+# Take,Cut,Peel,Put,Add,Mix
+subject2_file_315,fruitsalad,Take,Cut,Peel,Put,Add,Mix,
+
+# other,Mix
+subject2_file_679,fruitsalad,other,Mix,
+subject2_file_753,fruitsalad,other,Mix,
+
+# other,Add,Mix
+subject2_file_76,fruitsalad,other,Add,Mix,
+subject2_file_757,fruitsalad,other,Add,Mix,
+
+# Take,other,Add,Mix
+subject2_file_167,fruitsalad,Take,other,Add,Mix,
+subject2_file_213,fruitsalad,Take,other,Add,Mix,
+
+
+
+### subject3 ###
+
+## sandwich ##
+# Take
+subject3_file_14,sandwich,Take,
+subject3_file_188,sandwich,Take,
+subject3_file_204,sandwich,Take,
+subject3_file_254,sandwich,Take,
+subject3_file_314,sandwich,Take,
+subject3_file_789,sandwich,Take,
+subject3_file_881,sandwich,Take,
+subject3_file_986,sandwich,Take,
+
+# Put
+subject3_file_234,sandwich,Put,
+subject3_file_263,sandwich,Put,
+subject3_file_274,sandwich,Put,
+subject3_file_755,sandwich,Put,
+subject3_file_856,sandwich,Put,
+
+# Take,Wash
+subject3_file_165,sandwich,Take,Wash,
+subject3_file_418,sandwich,Take,Wash,
+subject3_file_715,sandwich,Take,Wash,
+subject3_file_851,sandwich,Take,Wash,
+
+# Cut,Wash
+subject3_file_134,sandwich,Cut,Wash,
+subject3_file_555,sandwich,Cut,Wash,
+subject3_file_775,sandwich,Cut,Wash,
+subject3_file_937,sandwich,Cut,Wash,
+
+# Take,Cut,Wash
+subject3_file_846,sandwich,Take,Cut,Wash,
+
+# Take,other
+subject3_file_463,sandwich,Take,other,
+
+# Put,other
+subject3_file_227,sandwich,Put,other,
+subject3_file_438,sandwich,Put,other,
+subject3_file_506,sandwich,Put,other,
+subject3_file_553,sandwich,Put,other,
+subject3_file_623,sandwich,Put,other,
+subject3_file_951,sandwich,Put,other,
+
+# Put,Cut,other
+subject3_file_483,sandwich,Put,Cut,other,
+subject3_file_536,sandwich,Put,Cut,other,
+subject3_file_679,sandwich,Put,Cut,other,
+subject3_file_680,sandwich,Put,Cut,other,
+subject3_file_953,sandwich,Put,Cut,other,
+
+# other,Cut,Wash
+subject3_file_641,sandwich,other,Cut,Wash,
+
+# other
+subject3_file_490,sandwich,other,
+
+
+## cereal ##
+# Take
+subject3_file_156,cereal,Take,
+subject3_file_347,cereal,Take,
+subject3_file_532,cereal,Take,
+subject3_file_638,cereal,Take,
+subject3_file_712,cereal,Take,
+subject3_file_814,cereal,Take,
+subject3_file_878,cereal,Take,
+
+# Take,Peel
+subject3_file_160,cereal,Take,Peel,
+subject3_file_451,cereal,Take,Peel,
+subject3_file_560,cereal,Take,Peel,
+subject3_file_698,cereal,Take,Peel,
+subject3_file_820,cereal,Take,Peel,
+
+# Take,Cut,Peel
+subject3_file_401,cereal,Take,Cut,Peel,
+
+# Put,Open,Cut,Peel
+subject3_file_654,cereal,Put,Open,Cut,Peel,
+
+# Pour,Put,Open,Cut
+subject3_file_663,cereal,Pour,Put,Open,Cut,
+
+# Pour,Open,Cut,Peel,Put
+subject3_file_120,cereal,Pour,Open,Cut,Peel,Put,
+subject3_file_419,cereal,Pour,Open,Cut,Peel,Put,
+subject3_file_466,cereal,Pour,Open,Cut,Peel,Put,
+subject3_file_914,cereal,Pour,Open,Cut,Peel,Put,
+
+# Put,other
+subject3_file_74,cereal,Put,other,
+
+# Pour,Put,other
+subject3_file_51,cereal,Pour,Put,other,
+subject3_file_155,cereal,Pour,Put,other,
+subject3_file_443,cereal,Pour,Put,other,
+subject3_file_457,cereal,Pour,Put,other,
+subject3_file_509,cereal,Pour,Put,other,
+
+
+## fruitsalad ##
+# Take
+subject3_file_220,fruitsalad,Take,
+subject3_file_239,fruitsalad,Take,
+subject3_file_277,fruitsalad,Take,
+subject3_file_684,fruitsalad,Take,
+subject3_file_724,fruitsalad,Take,
+subject3_file_912,fruitsalad,Take,
+
+# Peel
+subject3_file_499,fruitsalad,Peel,
+subject3_file_573,fruitsalad,Peel,
+subject3_file_693,fruitsalad,Peel,
+subject3_file_727,fruitsalad,Peel,
+subject3_file_758,fruitsalad,Peel,
+
+# Take,Peel
+subject3_file_159,fruitsalad,Take,Peel,
+subject3_file_479,fruitsalad,Take,Peel,
+subject3_file_501,fruitsalad,Take,Peel,
+subject3_file_587,fruitsalad,Take,Peel,
+subject3_file_611,fruitsalad,Take,Peel,
+subject3_file_616,fruitsalad,Take,Peel,
+
+# Cut,Peel
+subject3_file_550,fruitsalad,Cut,Peel,
+
+# Put,Cut,Peel
+subject3_file_39,fruitsalad,Put,Cut,Peel,
+subject3_file_55,fruitsalad,Put,Cut,Peel,
+subject3_file_113,fruitsalad,Put,Cut,Peel,
+subject3_file_284,fruitsalad,Put,Cut,Peel,
+subject3_file_596,fruitsalad,Put,Cut,Peel,
+subject3_file_639,fruitsalad,Put,Cut,Peel,
+subject3_file_662,fruitsalad,Put,Cut,Peel,
+subject3_file_852,fruitsalad,Put,Cut,Peel,
+subject3_file_853,fruitsalad,Put,Cut,Peel,
+subject3_file_908,fruitsalad,Put,Cut,Peel,
+subject3_file_923,fruitsalad,Put,Cut,Peel,
+
+# Take,Add,Mix
+subject3_file_935,fruitsalad,Take,Add,Mix,
+
+# Take,Put,Cut,Peel
+subject3_file_362,fruitsalad,Take,Put,Cut,Peel,
+subject3_file_482,fruitsalad,Take,Put,Cut,Peel,
+subject3_file_925,fruitsalad,Take,Put,Cut,Peel,
+
+# Take,Cut,Peel,Put,Add
+subject3_file_694,fruitsalad,Take,Cut,Peel,Put,Add,
+subject3_file_971,fruitsalad,Take,Cut,Peel,Put,Add,
+
+# Take,Cut,Peel,Put,Add,Mix
+subject3_file_77,fruitsalad,Take,Cut,Peel,Put,Add,Mix,
+
+# Take,other
+subject3_file_713,fruitsalad,Take,other,
+
+# other,Mix
+subject3_file_302,fruitsalad,other,Mix,
+
+# other,Add,Mix
+subject3_file_432,fruitsalad,other,Add,Mix,
+subject3_file_954,fruitsalad,other,Add,Mix,
+
+# Take,other,Add,Mix
+subject3_file_325,fruitsalad,Take,other,Add,Mix,
+subject3_file_632,fruitsalad,Take,other,Add,Mix,
+"""
+
+
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.svm import OneClassSVM
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.decomposition import PCA
+from scipy import signal
+from numpy.fft import fft, ifft, fftfreq
+from sklearn.cluster import KMeans
+from fastdtw import fastdtw
+import statsmodels.api as sm # 季節成分分解
+import seaborn as sns
+
+
+size = 145
+
+take = np.sum(np.loadtxt("subject1_file_119.csv", delimiter=",", skiprows=1)[:size], axis=1)
+take_fft = fft(take)
+  
+take_wash = np.sum(np.loadtxt("subject1_file_491.csv", delimiter=",", skiprows=1)[:size], axis=1)
+take_wash_fft = fft(take_wash)
+
+wash_fft = take_wash_fft - take_fft
+
+take_wash2 = np.sum(np.loadtxt("subject1_file_523.csv", delimiter=",", skiprows=1)[:size], axis=1)
+take_wash2_fft = fft(take_wash2)
+
+take_re_fft = take_wash2_fft - wash_fft
+
+take_re = ifft(take_re_fft)
+take_re = take_re.real
+
+take2 = np.sum(np.loadtxt("subject1_file_202.csv", delimiter=",", skiprows=1)[:size], axis=1)
+false = np.sum(np.loadtxt("subject1_file_712.csv", delimiter=",", skiprows=1)[:size], axis=1)
+
+
+plt.figure(figsize=(15, 9))
+plt.title('Trend')
+for ts, label, color in zip([take, take_re, take2, false], ["take", "take_re", "take2", "false"], ['blue', 'red', 'green', 'yellow']):
+    res = sm.tsa.seasonal_decompose(ts, freq=90)
+    plt.plot(res.trend, lw=.6, c=color, label=label)
+plt.legend()
+
+"""
+res_take = sm.tsa.seasonal_decompose(take, freq=90)
+res_take_re = sm.tsa.seasonal_decompose(take_re, freq=90)
+res_take2 = sm.tsa.seasonal_decompose(take2, freq=90)
+res_false = sm.tsa.seasonal_decompose(false, freq=90)
+
+distance, path = fastdtw(res_take.trend, res_take_re.trend)
+print(distance)
+plt.figure(figsize=(15, 9))
+plt.plot(res_take.trend, label="take", color="red")
+plt.plot(res_take_re.trend, label="take_re", color="blue")
+plt.title('Trend')
+plt.legend()
+for x_1, x_2 in path:
+  plt.plot([x_1, x_2], [res_take.trend[x_1], res_take_re.trend[x_2]], color='gray', linestyle='dotted', linewidth=1)
+plt.show()
+"""
