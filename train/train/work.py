f3141e0b52fc789298f4f27e54805d1eaaf24eff
a2h1r0 <atsu3.n9@gmail.com>
2020-02-21 13:03:56 +0900

    Update work.py

--- train/train/work.py
+++ train/train/work.py
@@ -15,21 +15,83 @@ import re
 import os
 import warnings
 import sys
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+
 
 
 file_num = ['3', '11', '21']
 directory = ['left_hip', 'right_arm', 'left_wrist', 'right_wrist']
 
 
-take_set = [[[] for i in range(len(file_num))] for i in range(len(directory))]
+# take_set[身体部位][ファイル番号*被験者1~3]
+
+take_data = [[] for i in range(len(directory))]
 for part, part_name in enumerate(directory):
-    for file_index, num in enumerate(file_num):
+    for num in file_num:
         path = glob.glob('.\\'+part_name+'\\subject?_file_'+num+'.csv')
         for filename in path:
-            try:data = np.loadtxt(filename, delimiter=",", skiprows=1, usecols=[0,1,2]) 
-            except IOError:continue
-            except UserWarning:continue
-            take_set[part][file_index].append([0 for i in range(6)])
-            take_set[part][file_index][-1] = list(np.concatenate([np.mean(data, axis=0), np.var(data, axis=0)]))
+            if os.path.isfile(filename):
+                data = np.loadtxt(filename, delimiter=",", skiprows=1, usecols=[0,1,2]) 
+                if data != []:
+                    take_data[part].append([0 for i in range(6)])
+                    take_data[part][-1] = list(np.concatenate([np.mean(data, axis=0), np.var(data, axis=0)]))
+                    
+        if take_set[part] == []:
+            del take_set[part]             
+
+# これで部位ごとに学習にかける．
+label = []
+for i in range(directory):
+    label.append(['take' for i in range(take_set)])
+clf = svm.SVC(C=1.0, kernel='linear')
+cross_val_score(clf, tale_data[part], label, cv=5)
+print('Cross-Validation scores: {}\n'.format(scores[-1]))
+        
+            
+                
+"""
+K-Means
+
+kmeans_model = KMeans(n_clusters=2, random_state=10).fit(left_hip_test)
+labels = kmeans_model.labels_
+print(labels)
+"""
 
 
+
+"""
+# PCA：いらんかった
+
+pca = PCA(n_components=2)
+pca.fit(train)
+pca_train = pca.components_
+pca.fit(test)
+pca_test = pca.components_
+
+x_axis = list(range(len(train)))
+plt.scatter(x_axis, pca_train)
+plt.scatter(x_axis, pca_test)   
+"""
+    
+
+"""
+# OneClassSVM：ダメダメ
+
+clf = OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+clf.fit(train)
+pred = clf.predict(test)
+print(pred)
+"""    
+
+
+"""
+# LOF：ダメダメ
+
+model = LocalOutlierFactor(n_neighbors=1,
+                           novelty=True,
+                           contamination=0.1)
+model.fit(train) # train_dataは正常データが大多数であるような訓練データ
+prediction = model.predict(test) # テストデータに対する予測
+print(prediction)
+"""
