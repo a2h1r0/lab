03a0de5bad4064c14b45a3fdfa53367b59e2ff11
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-12-21 22:50:04 +0900

    Interaction2021 更新

--- research/pulse/UWW2020/pulse_show_csv_5sec_2row.py
+++ research/pulse/UWW2020/pulse_show_csv_5sec_2row.py
@@ -6,65 +6,60 @@ from scipy.signal import find_peaks
 import os
 os.chdir(os.path.dirname(__file__))
 
-FILE1 = '20201211_205544_raw_pulsesensor.csv'
-FILE2 = '20201211_213045_pseudo_pulsesensor.csv'
-FIG_NAME = 'pulsesensor.svg'
-
-START1 = 5.067196
-END1 = 10.067196
-START2 = 7.332872
-END2 = 12.332872
+file1 = 'raw_peak_start.csv'
+file2 = 'pseudo_peak_start.csv'
 
 t1 = []
 y1 = []
-with open(FILE1) as f:
+start = 0
+with open(file1) as f:
+
     reader = csv.reader(f)
     next(reader)
 
     for row in reader:
-        # 開始時間まで待機
-        if float(row[0]) < START1:
-            continue
-        # 終了時間で終了
-        elif END1 < float(row[0]):
+        # 開始時間の保存
+        if start == 0:
+            start = float(row[0])
+
+        # 5秒経過で終了
+        if start + 5 < float(row[0]):
             break
 
         # 0秒を開始点にする
-        t1.append(float(row[0])*1000 - START1*1000)
+        t1.append(float(row[0])*1000 - start*1000)
         y1.append(float(row[1]))
 
 t2 = []
 y2 = []
 start = 0
-with open(FILE2) as f:
+with open(file2) as f:
+
     reader = csv.reader(f)
     next(reader)
 
     for row in reader:
-        # 開始時間まで待機
-        if float(row[0]) < START2:
-            continue
-        # 終了時間で終了
-        elif END2 < float(row[0]):
+        # 開始時間の保存
+        if start == 0:
+            start = float(row[0])
+
+        # 5秒経過で終了
+        if start + 5 < float(row[0]):
             break
 
         # 0秒を開始点にする
-        t2.append(float(row[0])*1000 - START2*1000)
+        t2.append(float(row[0])*1000 - start*1000)
         y2.append(float(row[1]))
 
 
-fig = plt.figure(figsize=(16, 9))
-ax1 = fig.add_subplot(111)
-ax1.plot(t1, y1, 'red', label="Raw pulse value")
-ax2 = ax1.twinx()
-ax2.plot(t2, y2, 'blue', linestyle="dashed", label="Pseudo pulse value")
+plt.figure(figsize=(16, 9))
+plt.plot(t1, y1, 'red', label="Subject")
+plt.plot(t2, y2, 'blue', linestyle="dashed", label="Generated")
+plt.xlabel("Time [ms]", fontsize=18)
+plt.ylabel("Pulse sensor value", fontsize=18)
+plt.tick_params(labelsize=18)
+plt.legend(fontsize=18, loc='upper right')
+plt.savefig("pulse.svg", bbox_inches='tight', pad_inches=0)
+plt.savefig("pulse.eps", bbox_inches='tight', pad_inches=0)
 
-h1, l1 = ax1.get_legend_handles_labels()
-h2, l2 = ax2.get_legend_handles_labels()
-ax1.legend(h1+h2, l1+l2, fontsize=18, loc='upper right')
-ax1.set_xlabel("Time [ms]", fontsize=18)
-ax1.set_ylabel("Raw pulse value", fontsize=18)
-ax2.set_ylabel("Pseudo pulse value", fontsize=18)
-
-plt.savefig(FIG_NAME, bbox_inches='tight', pad_inches=0)
 plt.show()
