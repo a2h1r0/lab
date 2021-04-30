d30eac1c99bfab035ca3932e7270d87054fcaa91
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-04-30 22:05:46 +0900

    実行時間の記録機能を実装

--- research/pulse/work/make_heart_rate_watch.py
+++ research/pulse/work/make_heart_rate_watch.py
@@ -1,24 +1,29 @@
 import numpy as np
 import csv
+import datetime
 import socket
 import time
 import os
 os.chdir(os.path.dirname(__file__))
 
 
+PROCESS_TIME = 240  # 実行時間
+LOG_FILE = './data/run.log'  # ログファイル
+
+
 SOCKET_ADDRESS = '192.168.11.2'  # Processingサーバのアドレス
 SOCKET_PORT = 10000  # Processingサーバのポート
 
 
-TRAIN_DATA = '20210228_124511_raw'
-PROCESS_TIME = 240
+def light(heart_rate):
+    """点灯
 
-
-def light():
-    """点灯"""
+        Args:
+            heart_rate (int): 再現する心拍数
+    """
 
     def make_display_data(heart_rate):
-        """ランダム色データの生成
+        """色データの生成
 
         Args:
             heart_rate (int): 再現する心拍数
@@ -27,13 +32,12 @@ def light():
             list: 点灯時間
         """
 
-        # sin波（0 ~ 2）の生成
-        sin = np.sin(np.linspace(0, 2 * np.pi, 10)) + 1
-        # 1以上の部分を1にする（0 ~ 1）
-        sin[sin > 1] = 1
+        # sin波（0 ~ 1）の生成
+        sin = np.sin(np.linspace(np.pi, 2 * np.pi, 10)) + 1
+        ones = np.ones(10)
 
         # グレースケールへ変換
-        single_wave = sin * 30 + 120
+        single_wave = np.append(ones, sin) * 30 + 225
 
         wave = []
         for i in range(heart_rate):
@@ -46,12 +50,10 @@ def light():
 
         return colors, lighting_time
 
-    heart_rate = input('心拍数は？ > ')
-
     # 色データの作成
     colors, lighting_time = make_display_data(int(heart_rate))
 
-    # 開始時間の取得
+    # 描画開始時間の取得
     start = time.time()
 
     # 色データの描画
@@ -81,5 +83,19 @@ if __name__ == '__main__':
     socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_client.connect((SOCKET_ADDRESS, SOCKET_PORT))
 
+    # 心拍数の設定
+    heart_rate = input('\n\n心拍数は？ > ')
+
+    # プログラム実行日時と実行時間，心拍数を記録
+    with open(LOG_FILE, 'a', newline='') as log_file:
+        log_writer = csv.writer(log_file, delimiter=',')
+        now = datetime.datetime.today()
+        date_time = now.strftime('%Y/%m/%d') + ' ' + now.strftime('%H:%M:%S')
+        log_writer.writerow([date_time, PROCESS_TIME, heart_rate])
+
+    print('\n描画中．．．')
+
     # 点灯開始
-    light()
+    light(heart_rate)
+
+    print('\n----- 描画終了 -----\n')
