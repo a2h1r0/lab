5ffce570a2053c390e84a9d64f399a89b22563f3
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-05-01 18:15:41 +0900

    データ描画プログラムの作成

--- research/pulse/work/make_heart_rate_watch.py
+++ research/pulse/work/make_heart_rate_watch.py
@@ -7,7 +7,7 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 
-PROCESS_TIME = 240  # 実行時間
+PROCESS_TIME = 130  # 実行時間（アプリ側のデータ取得は120秒間）
 LOG_FILE = './data/run.log'  # ログファイル
 
 
---
+++ research/pulse/work/show_heart_rate.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import csv
+from natsort import natsorted
+import glob
+import os
+os.chdir(os.path.dirname(__file__))
+
+
+MODEL = 'TicWatch'
+TARGET_RATES = [70, 75, 80, 85, 90, 95, 100]
+DISPLAYS = ['laptop', 'laptop']
+COLORS = ['red', 'blue']
+
+
+def main():
+    # グラフの用意
+    plt.figure(figsize=(16, 9))
+    plt.xlabel('Target Heart Rate', fontsize=18)
+    plt.ylabel('Diff', fontsize=18)
+    plt.title(MODEL, fontsize=18)
+    plt.tick_params(labelsize=18)
+
+    # ディスプレイごとにデータを描画
+    for index, (display, color) in enumerate(zip(DISPLAYS, COLORS)):
+        files = natsorted(glob.glob('./data/' + MODEL +
+                                    '/' + display + '/*_HeartRate_*.csv'))
+
+        if len(TARGET_RATES) != len(files):
+            print('\nパラメータに誤りがあります。\n')
+            break
+
+        # 心拍数ごとにデータを取得
+        diffs = []
+        for target_rate, data in zip(TARGET_RATES, files):
+            with open(data) as f:
+                reader = csv.reader(f)
+                next(reader)
+
+                values = []
+                for row in reader:
+                    values.append(int(row[1]))
+
+                # 取得時間での平均値
+                average = round(sum(values) / len(values))
+                # 目標値からの差の計算
+                diffs.append(average - target_rate)
+
+        # グラフの描画
+        plt.plot(TARGET_RATES, diffs, color, label=display)
+
+    plt.legend(fontsize=18, loc='upper right')
+    # plt.savefig('../figure/' + MODEL + '.png',
+    #             bbox_inches='tight', pad_inches=0)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
