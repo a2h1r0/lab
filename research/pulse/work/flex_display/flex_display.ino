6aa4019d4436115e5ba116d76e245ffa597a8b8b
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-05-15 14:56:32 +0900

    進捗

--- research/pulse/work/flex_display/flex_display.ino
+++ research/pulse/work/flex_display/flex_display.ino
@@ -1,11 +1,18 @@
+// 目標心拍数
+const int H_TARGET = 50;
+
 // 使用ピン
 const int PWM = 3;
 // 黒の電圧値 (V)
 const int BLACK = 5;
 // 白の電圧値 (V)
 const int WHITE = 0;
+// 色データ長
+const int L_COLORS = 6;
+// 色データ
+const int COLORS[L_COLORS] = {WHITE, WHITE, BLACK / 2, BLACK, BLACK / 2, WHITE};
 // 描画間隔
-const int DELAY = 500;
+const int DELAY = 60 / (L_COLORS * H_TARGET);
 
 /**
  * @fn
@@ -22,8 +29,9 @@ void setup()
  */
 void loop()
 {
-    analogWrite(PWM, BLACK * 51);
-    delay(DELAY);
-    analogWrite(PWM, WHITE * 51);
-    delay(DELAY);
+    for (int i = 0; i < L_COLORS; i++)
+    {
+        analogWrite(PWM, COLORS[i] * 51);
+        delay(DELAY);
+    }
 }
