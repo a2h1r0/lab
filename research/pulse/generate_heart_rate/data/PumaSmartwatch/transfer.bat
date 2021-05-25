8405d5829af95fded832f74595b429d70cd188a2
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-05-25 16:05:43 +0900

    転送プログラムの更新

---
+++ research/pulse/generate_heart_rate/data/PumaSmartwatch/transfer.bat
@@ -0,0 +1,4 @@
+adb -s 192.168.11.59:5555 pull /storage/self/primary/Android/data/com.example.getpulse/files ./
+adb -s 192.168.11.59:5555 shell rm -rf /storage/self/primary/Android/data/com.example.getpulse/files
+move .\files\* .\
+rmdir .\files
--- research/pulse/generate_heart_rate/data/TicWatch/transfer.bat
+++ research/pulse/generate_heart_rate/data/TicWatch/transfer.bat
@@ -1,4 +1,4 @@
-adb pull /storage/self/primary/Android/data/com.example.getpulse/files ./
-adb shell rm -rf /storage/self/primary/Android/data/com.example.getpulse/files
+adb -s 192.168.11.57:5555 pull /storage/self/primary/Android/data/com.example.getpulse/files ./
+adb -s 192.168.11.57:5555 shell rm -rf /storage/self/primary/Android/data/com.example.getpulse/files
 move .\files\* .\
 rmdir .\files
