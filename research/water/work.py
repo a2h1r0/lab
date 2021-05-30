68505ca26acb8c663535cd76510be0ac17126623
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-05-30 14:06:56 +0900

    FFTプログラムの作成

--- research/water/work.py
+++ research/water/work.py
@@ -4,63 +4,31 @@ import matplotlib.pyplot as plt
 import os
 os.chdir(os.path.dirname(__file__))
 
-sound_file = './sounds/test.mp3'
 
-sound = AudioSegment.from_file(sound_file, 'mp3')
+SOUND_DATA = './sounds/test.mp3'  # 音源
+
+
+# ファイルの読み出し
+sound = AudioSegment.from_file(SOUND_DATA, 'mp3')
+
+# データの整形
 data = np.array(sound.get_array_of_samples())
-# spec = np.fft.fft(data)  # 2次元配列(実部，虚部)
-# freq = np.fft.fftfreq(data.shape[0], 1.0/sound.frame_rate)
-# spec = spec[:int(spec.shape[0]/2 + 1)]  # 周波数がマイナスになるスペクトル要素の削除
-# freq = freq[:int(freq.shape[0]/2 + 1)]  # 周波数がマイナスになる周波数要素の削除
-# max_spec = max(np.abs(spec))  # 最大音圧を取得(音圧を正規化するために使用）
-# plt.plot(freq, np.abs(spec)/max_spec)
+sample_num = len(data)
+sampling_rate = sound.frame_rate
 
-# plt.grid()
-# plt.xlim([0, 4000])  # グラフに出力する周波数の範囲[Hz]
-# plt.xlabel("Frequency[Hz]")
-# plt.ylabel("Sound Pressure[-]")
-# plt.yscale("log")
-# plt.savefig(file + ".png")  # pngファイルで出力
+# 周波数の取得
+freqs = np.fft.fftfreq(sample_num, d=1.0/sampling_rate)
+# FFT
+fft = np.abs(np.fft.fft(data))
 
 
-# データのパラメータ
-N = 256            # サンプル数
-dt = 0.01          # サンプリング間隔
-f1, f2 = 10, 20    # 周波数
-# t = np.arange(0, N*dt, dt)  # 時間軸
-freq = np.linspace(0, 1.0/dt, N)  # 周波数軸
+plt.figure(figsize=(16, 9))
+plt.plot(freqs, fft, 'blue')
+plt.xlabel('Frequency [Hz]', fontsize=18)
+plt.ylabel('Amplitude', fontsize=18)
+plt.tick_params(labelsize=18)
+plt.xlim(0, 8000)
+plt.ticklabel_format(style='plain', axis='y')
+# plt.savefig('../figure/power.png', bbox_inches='tight', pad_inches=0)
 
-# 信号を生成（周波数10の正弦波+周波数20の正弦波+ランダムノイズ）
-# f = np.sin(2*np.pi*f1*t) + np.sin(2*np.pi*f2*t) + 0.3 * np.random.randn(N)
-
-# 高速フーリエ変換
-F = np.fft.fft(data)
-rate = sound.frame_rate
-time = sound.duration_seconds
-t = np.arange(0, time, rate)  # 時間軸
-
-# 振幅スペクトルを計算
-Amp = np.abs(F)
-
-# パワースペクトルの計算（振幅スペクトルの二乗）
-Pow = Amp ** 2
-
-# グラフ表示
-plt.figure()
-plt.rcParams['font.family'] = 'Times New Roman'
-plt.rcParams['font.size'] = 17
-plt.subplot(121)
-# plt.plot(rate, f, label='f(n)')
-# plt.xlabel("Time", fontsize=20)
-# plt.ylabel("Signal", fontsize=20)
-plt.grid()
-leg = plt.legend(loc=1, fontsize=25)
-leg.get_frame().set_alpha(1)
-plt.subplot(122)
-plt.plot(freq, Pow, label='|F(k)|')
-plt.xlabel('Frequency', fontsize=20)
-plt.ylabel('Amplitude', fontsize=20)
-plt.grid()
-leg = plt.legend(loc=1, fontsize=25)
-leg.get_frame().set_alpha(1)
 plt.show()
