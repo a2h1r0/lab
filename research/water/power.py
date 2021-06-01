c8887fb9eba86f3b04aab3e6dfccf332f9f837a2
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-02 00:00:36 +0900

    Update power.py

--- research/water/power.py
+++ research/water/power.py
@@ -5,12 +5,18 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 
-SOUND_DATA = './sounds/washer.mp3'  # 音源
-WINDOW = 5  # 末尾から何秒分表示するか
+SOUND_FILE = 'shampoo_1.mp3'  # 音源
+
+
+SOUND_DIR = './sounds/'
 
 
 # ファイルの読み出し
-sound = AudioSegment.from_file(SOUND_DATA, 'mp3')
+sound = AudioSegment.from_file(SOUND_DIR + SOUND_FILE, 'mp3')
+
+plt.xlabel('Frequency [Hz]', fontsize=18)
+plt.ylabel('Amplitude', fontsize=18)
+plt.tick_params(labelsize=18)
 
 # データの整形
 data = np.array(sound.get_array_of_samples())
@@ -29,9 +35,6 @@ power = fft ** 2
 
 plt.figure(figsize=(16, 9))
 plt.plot(freqs[index], power[index], 'blue')
-plt.xlabel('Frequency [Hz]', fontsize=18)
-plt.ylabel('Amplitude', fontsize=18)
-plt.tick_params(labelsize=18)
 plt.xlim(0, max(freqs))
 # plt.savefig('../figure/power.png', bbox_inches='tight', pad_inches=0)
 
