69b2c379e7d2002d201a418a1e62704d755dbcea
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-01 18:30:47 +0900

    Update work.py

--- research/water/work.py
+++ research/water/work.py
@@ -1,4 +1,5 @@
 from pydub import AudioSegment
+from pydub.silence import split_on_silence
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -11,16 +12,28 @@ SOUND_DATA = './sounds/washer.mp3'  # 音源
 # ファイルの読み出し
 sound = AudioSegment.from_file(SOUND_DATA, 'mp3')
 
+
+chunks = split_on_silence(sound, min_silence_len=2000, silence_thresh=-40)
+
 # データの整形
 data = np.array(sound.get_array_of_samples())
 sample_num = len(data)
-
-
 plt.figure(figsize=(16, 9))
+
 plt.plot(range(sample_num), data, 'blue')
+
 plt.xlabel('Time [s]', fontsize=18)
 plt.ylabel('Sound', fontsize=18)
 plt.tick_params(labelsize=18)
-# plt.savefig('../figure/power.png', bbox_inches='tight', pad_inches=0)
+
+for chunk in chunks:
+    # データの整形
+    data = np.array(chunk.get_array_of_samples())
+    sample_num = len(data)
+    plt.figure(figsize=(16, 9))
+
+    plt.plot(range(sample_num), data, 'blue')
+    # plt.savefig('../figure/power.png', bbox_inches='tight', pad_inches=0)
+
 
 plt.show()
