424d871c5964facefb9fa08f08fa99cbabfdf347
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-01 23:25:17 +0900

    update

--- research/water/work.py
+++ research/water/work.py
@@ -6,34 +6,39 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 
-SOUND_DATA = './sounds/washer.mp3'  # 音源
+SOUND_DIR = './sounds/'
+SOUND_FILE = 'shampoo_1.mp3'  # 音源
 
 
 # ファイルの読み出し
-sound = AudioSegment.from_file(SOUND_DATA, 'mp3')
+sound = AudioSegment.from_file(SOUND_DIR + 'raw/' + SOUND_FILE, 'mp3')
 
-
-chunks = split_on_silence(sound, min_silence_len=2000, silence_thresh=-40)
-
-# データの整形
-data = np.array(sound.get_array_of_samples())
-sample_num = len(data)
-plt.figure(figsize=(16, 9))
-
-plt.plot(range(sample_num), data, 'blue')
 
 plt.xlabel('Time [s]', fontsize=18)
 plt.ylabel('Sound', fontsize=18)
 plt.tick_params(labelsize=18)
 
-for chunk in chunks:
+# データの整形
+data = np.array(sound.get_array_of_samples())
+sample_num = len(data)
+
+plt.figure(figsize=(16, 9))
+plt.title('Overview')
+plt.plot(range(sample_num), data)
+
+# 分割
+chunks = split_on_silence(sound, min_silence_len=2000,
+                          silence_thresh=-55)
+for index, chunk in enumerate(chunks):
     # データの整形
     data = np.array(chunk.get_array_of_samples())
     sample_num = len(data)
-    plt.figure(figsize=(16, 9))
-
-    plt.plot(range(sample_num), data, 'blue')
-    # plt.savefig('../figure/power.png', bbox_inches='tight', pad_inches=0)
-
+    plt.figure(index, figsize=(16, 9))
+    plt.title('Split')
+    plt.plot(range(sample_num), data)
 
 plt.show()
+
+
+# 切り出し部分の保存
+chunks[0].export(SOUND_DIR + SOUND_FILE, format="mp3")
