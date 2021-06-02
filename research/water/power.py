f5d85736bd15300c67a8bf4c7745c09d60f458a6
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-02 22:05:46 +0900

    Update power.py

--- research/water/power.py
+++ research/water/power.py
@@ -6,6 +6,7 @@ os.chdir(os.path.dirname(__file__))
 
 
 SOUND_FILE = 'shampoo_1.mp3'  # 音源
+STEP = 3
 
 
 SOUND_DIR = './sounds/'
@@ -18,24 +19,33 @@ plt.xlabel('Frequency [Hz]', fontsize=18)
 plt.ylabel('Amplitude', fontsize=18)
 plt.tick_params(labelsize=18)
 
-# データの整形
-data = np.array(sound.get_array_of_samples())
-data = data[-(WINDOW*1000):]
-sample_num = len(data)
-sampling_rate = sound.frame_rate
 
-# 周波数の取得
-freqs = np.fft.fftfreq(sample_num, d=1.0/sampling_rate)
-index = np.argsort(freqs)
-# 振幅スペクトル
-fft = np.abs(np.fft.fft(data))
-# パワースペクトル
-power = fft ** 2
+sound_len = sound.duration_seconds
+for i in range(int(sound_len // STEP)):
+    start = i * STEP
+    if i == (int(sound_len // STEP) - 1):
+        show_sound = sound[start:]
+    else:
+        end = (i + 1) * STEP
+        show_sound = sound[start:end]
+
+    data = np.array(show_sound.get_array_of_samples())
+    sample_num = len(data)
+    sampling_rate = sound.frame_rate
+
+    # 周波数の取得
+    freqs = np.fft.fftfreq(sample_num, d=1.0/sampling_rate)
+    index = np.argsort(freqs)
+    # 振幅スペクトル
+    fft = np.abs(np.fft.fft(data))
+    # パワースペクトル
+    power = fft ** 2
+
+    plt.figure(i, figsize=(16, 9))
+    plt.title(str(start) + 's to ' + str(start + STEP) + 's')
+    plt.plot(freqs[index], power[index], 'blue')
+    plt.xlim(0, max(freqs))
 
 
-plt.figure(figsize=(16, 9))
-plt.plot(freqs[index], power[index], 'blue')
-plt.xlim(0, max(freqs))
 # plt.savefig('../figure/power.png', bbox_inches='tight', pad_inches=0)
-
 plt.show()
