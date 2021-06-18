d067d1f49ab716229263c83532c45a295057d810
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-18 20:23:08 +0900

    ファイルの作成

--- research/pulse/generate_heart_rate/flex_display/flex_display.py
+++ research/pulse/generate_heart_rate/flex_display/flex_display.py
@@ -7,7 +7,7 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 
-MODEL = 'TicWatch'  # スマートウォッチのモデル
+MODEL = 'SMART_R'  # スマートウォッチのモデル
 PROCESS_TIME = 40  # 実行時間（アプリ側のデータ取得は120秒間）
 # PROCESS_TIMEはArduino側のプログラムでも設定する必要あり
 
