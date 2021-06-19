b6838169a4e14f4dfe36e262ffaf21b83904a267
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-19 15:15:33 +0900

    Update show_heart_rate_applewatch.py

--- research/pulse/paper/show_heart_rate_applewatch.py
+++ research/pulse/paper/show_heart_rate_applewatch.py
@@ -7,7 +7,7 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 
-MODEL = 'Series_5'  # 表示するスマートウォッチ
+MODEL = 'Series_3'  # 表示するスマートウォッチ
 TARGET_RATES = [60, 65, 70, 75, 80, 85, 90, 95, 100]  # 取得した目標心拍数
 DISPLAYS = [['Legion7', 'Display A'], ['OSOYOO', 'Display B'],
             ['KeDei', 'Display C']]  # 表示するディスプレイ
@@ -19,11 +19,11 @@ SENSING_TIME = 60  # データを使用する時間
 
 def main():
     for display in DISPLAYS:
-        for directory in DIRS:
+        # 取得回数ごとの配列を作成
+        diffs = [[] for i in range(len(DIRS))]
+        for index, directory in enumerate(DIRS):
             files = natsorted(glob.glob('../generate_heart_rate/data/AppleWatch/' +
                                         MODEL + '/' + display[0] + '/' + directory + '/*.csv'))
-            diffs = []
-            sample_num = []
 
             if len(TARGET_RATES) != len(files):
                 print('\nパラメータに誤りがあります。\n')
@@ -47,18 +47,16 @@ def main():
                     # 取得時間での平均値
                     get_rate = sum(values) / len(values)
                     # 目標値からの差の計算
-                    diffs.append(get_rate - target_rate)
-                    # サンプル数の追加
-                    sample_num.append(len(values))
+                    diffs[index].append(get_rate - target_rate)
 
-            # 結果の表示
-            print('\n--- ' + display[1] + ' / ' + directory + ' ---\n')
-            for target, diff in zip(TARGET_RATES, diffs):
-                print(str(target) + ': ' + str(round(diff, 3)))
-            print('\nAverage Diff: ' + str(round(np.mean(diffs), 3)))
-            print('\n*** Sample Length ***')
-            print('Min: ' + str(min(sample_num)))
-            print('Max: ' + str(max(sample_num)) + '\n')
+        # 3回の平均を計算
+        averages = np.mean(diffs, axis=0)
+
+        # 結果の表示
+        print('\n--- ' + display[1] + ' ---\n')
+        for target, diff in zip(TARGET_RATES, averages):
+            print(str(target) + ': ' + str(round(diff, 3)))
+        print('\nAverage Diff: ' + str(round(np.mean(averages), 3)))
 
 
 if __name__ == '__main__':
