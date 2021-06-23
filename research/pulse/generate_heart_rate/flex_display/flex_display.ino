08ca17e6c949799893f9ae63abdebbb87f2e5f8c
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-23 22:52:31 +0900

    Update flex_display.ino

--- research/pulse/generate_heart_rate/flex_display/flex_display.ino
+++ research/pulse/generate_heart_rate/flex_display/flex_display.ino
@@ -1,3 +1,5 @@
+#include <cppQueue.h>
+
 // 実行時間
 const int PROCESS_TIME = 40;
 
@@ -17,9 +19,11 @@ const int COLORS_PWM_1[L_COLORS] = {WHITE_PWM_1, BLACK_PWM_1};
 const int COLORS_PWM_2[L_COLORS] = {WHITE_PWM_2, BLACK_PWM_2};
 // 目標心拍数受け取り用配列
 char heart_rate_data[4] = {'\0'};
+int i = 0;
+unsigned long drew_time;
+cppQueue peaks(sizeof(unsigned long), 5);
 
 /**
- * @fn
  * 初期化
  */
 void setup()
@@ -27,50 +31,100 @@ void setup()
     pinMode(PWM_1, OUTPUT);
     pinMode(PWM_2, OUTPUT);
     Serial.begin(9600);
+
+    int x = 10;
+    peaks.push(&x);
+    int *get;
+    peaks.pop(get);
+    Serial.println(*get, DEC);
+    x++;
+    peaks.push(&x);
+    x++;
+    peaks.push(&x);
+    x++;
+    peaks.push(&x);
+    x++;
+    peaks.push(&x);
 }
 
 /**
- * @fn
+ * 心拍数の取得
+ * 
+ * @return 心拍数
+ */
+int get_pulse()
+{
+    int pulse = analogRead(A0);
+
+    return 60;
+}
+
+/**
+ * 心拍数の取得
+ * 
+ * @return 心拍数
+ */
+int get_heart_rate()
+{
+    // Serial.println(analogRead(A0));
+
+    return 60;
+}
+
+/**
+ * 点灯時間の計算
+ * 
+ * @param heart_rate 心拍数
+ * @return 点灯時間
+ */
+unsigned long get_lighting_time(int heart_rate)
+{
+    // 点灯時間の計算
+    float time = (float)60 / (L_COLORS * heart_rate);
+    // マイクロ秒変換
+    int lighting_time = time * 1000000;
+
+    return lighting_time;
+}
+
+/**
+ * ディスプレイの描画
+ */
+void draw_display()
+{
+    // 描画
+    analogWrite(PWM_1, COLORS_PWM_1[i] * 51);
+    analogWrite(PWM_2, COLORS_PWM_2[i] * 51);
+
+    // 描画した時刻を保存
+    drew_time = micros();
+
+    // Colorsを更新
+    if (i == L_COLORS - 1)
+    {
+        i = 0;
+    }
+    else
+    {
+        i++;
+    }
+}
+
+/**
  * メインループ
  */
 void loop()
 {
-    // 黒くした状態で待機
-    analogWrite(PWM_1, BLACK_PWM_1 * 51);
-    analogWrite(PWM_2, BLACK_PWM_2 * 51);
+    // 心拍数の取得
+    int heart_rate = get_heart_rate();
+    // 点灯時間の計算
+    unsigned long lighting_time = get_lighting_time(heart_rate);
 
-    if (Serial.available())
+    // 現在時刻の取得
+    unsigned long now = micros();
+    // 描画時間の経過で再描画
+    if (now > drew_time + lighting_time)
     {
-        // 目標心拍数の取得
-        Serial.readStringUntil('\0').toCharArray(heart_rate_data, sizeof heart_rate_data);
-        int heart_rate = atoi(heart_rate_data);
-        // 点灯時間の計算
-        float lighting_time = (float)60 / (L_COLORS * heart_rate);
-        // ミリ秒変換
-        int delay_time = lighting_time * 1000;
-
-        // 描画開始時間の取得
-        long start = micros();
-
-        // 描画
-        while (true)
-        {
-            // 時間経過で終了
-            long process = micros() - start;
-            if (process > (PROCESS_TIME * 1000000))
-            {
-                break;
-            }
-
-            for (int i = 0; i < L_COLORS; i++)
-            {
-                analogWrite(PWM_1, COLORS_PWM_1[i] * 51);
-                analogWrite(PWM_2, COLORS_PWM_2[i] * 51);
-                delay(delay_time);
-            }
-        }
-
-        // 描画完了通知の送信
-        Serial.println(0);
+        draw_display();
     }
 }
