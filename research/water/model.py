de24d874a19484b81cc93acd58aca2dd0fe83d3f
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-24 21:30:05 +0900

    モデルの動作完了

--- research/water/model.py
+++ research/water/model.py
@@ -10,55 +10,41 @@ class Net(nn.Module):
         kernel_size (int): カーネルサイズ
     """
 
-    def __init__(self, kernel_size=15):
+    def __init__(self, kernel_size=7):
         super().__init__()
 
-        ###--- Encoder ---###
         self.conv1 = nn.Conv1d(
             in_channels=1, out_channels=8, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
-        self.relu1 = nn.LeakyReLU(negative_slope=0.01, inplace=True)
+        self.pool1 = nn.MaxPool1d(kernel_size=kernel_size)
 
         self.conv2 = nn.Conv1d(
             in_channels=8, out_channels=16, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
-        self.relu2 = nn.LeakyReLU(negative_slope=0.01, inplace=True)
+        self.pool2 = nn.MaxPool1d(kernel_size=kernel_size)
 
         self.conv3 = nn.Conv1d(
             in_channels=16, out_channels=32, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
-        self.relu3 = nn.LeakyReLU(negative_slope=0.01, inplace=True)
+        self.pool3 = nn.AdaptiveAvgPool1d(1)
 
-        self.conv4 = nn.Conv1d(
-            in_channels=32, out_channels=64, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
-        self.relu4 = nn.LeakyReLU(negative_slope=0.01, inplace=True)
-
-        self.conv5 = nn.Conv1d(
-            in_channels=64, out_channels=128, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
-        self.relu5 = nn.LeakyReLU(negative_slope=0.01, inplace=True)
+        self.fc1 = nn.Linear(32, 1)
 
     def forward(self, input):
         """
         Args:
-            input (:obj:`Tensor`[1, 2, 256]): 正解 or 生成 色データ + 脈波データのペア
+            input (:obj:`Tensor`[batch_size, 1, WINDOW_SIZE]): 音源データ
         Returns:
-            :obj:`Tensor`[1, 1, 256]: 識別結果
+            :obj:`Tensor`[batch_size, 1]: 識別結果
         """
 
-        ###--- Encoder ---###
         conv1_out = self.conv1(input)
-        x1 = self.relu1(conv1_out)
+        pool1_out = self.pool1(conv1_out)
 
-        conv2_out = self.conv2(x1)
-        x2 = self.relu2(conv2_out)
+        conv2_out = self.conv2(pool1_out)
+        pool2_out = self.pool2(conv2_out)
 
-        conv3_out = self.conv3(x2)
-        x3 = self.relu3(conv3_out)
+        conv3_out = self.conv3(pool2_out)
+        pool3_out = self.pool3(conv3_out)
 
-        conv4_out = self.conv4(x3)
-        x4 = self.relu4(conv4_out)
-
-        conv5_out = self.conv5(x4)
-        x5 = self.relu5(conv5_out)
-
-        # グレースケール化（0 ~ 255）
-        out = torch.round(self.hardtanh(conv10_out))
+        pool3_out = pool3_out.view(pool3_out.size(0), -1)
+        out = self.fc1(pool3_out)
 
         return out
--- research/water/work.py
+++ research/water/work.py
@@ -1,14 +1,43 @@
 import numpy as np
+from pydub import AudioSegment
 import torch
 import torch.nn as nn
 import torch.optim as optimizers
 from model import Net
+import matplotlib.pyplot as plt
 import os
 os.chdir(os.path.dirname(__file__))
 
 
-EPOCH_NUM = 10000  # 学習サイクル数
-KERNEL_SIZE = 13  # カーネルサイズ（奇数のみ）
+SOUND_DIR = './sounds/'
+SOUND_FILE = 'shampoo_1.mp3'  # 音源
+
+EPOCH_NUM = 100  # 学習サイクル数
+KERNEL_SIZE = 7  # カーネルサイズ（奇数のみ）
+WINDOW_SIZE = 100000
+STEP = 10000
+
+
+def make_train_data():
+    """
+    学習データの作成
+    """
+
+    # 音源の読み出し
+    sound = AudioSegment.from_file(SOUND_DIR + SOUND_FILE, 'mp3')
+
+    # データの整形
+    data = np.array(sound.get_array_of_samples())
+    labels = np.linspace(0, 100, len(data))
+
+    train_data, train_labels = [], []
+    for index in range(0, len(data)-WINDOW_SIZE+1, STEP):
+        start = index
+        end = index + WINDOW_SIZE - 1
+        train_data.append(data[start:end+1])
+        train_labels.append(labels[end])
+
+    return train_data, train_labels
 
 
 def main():
@@ -17,7 +46,16 @@ def main():
         モデルの学習
         """
 
+        # 学習データの作成
+        train_data, train_labels = make_train_data()
+        # Tensorへ変換
+        inputs = torch.tensor(
+            train_data, dtype=torch.float, device=device).view(-1, 1, WINDOW_SIZE)
+        labels = torch.tensor(
+            train_labels, dtype=torch.float, device=device).view(-1, 1)
+
         model.train()
+        print('\n***** 学習開始 *****')
 
         '''学習サイクル'''
         for epoch in range(EPOCH_NUM):
@@ -27,9 +65,11 @@ def main():
             loss.backward()
             optimizer.step()
 
-            if epoch % 100 == 0:
+            if (epoch + 1) % 10 == 0:
                 print(
-                    '\nEpoch: {:3d} / Loss: {:.3f}'.format(epoch+1, loss.item()))
+                    'Epoch: {:d} / Loss: {:.3f}'.format(epoch + 1, loss.item()))
+
+        print('\n----- 終了 -----\n')
 
     def test():
         """
@@ -40,8 +80,8 @@ def main():
         # テスト処理
 
     # モデルの構築
-    model = Net(kernel_size=KERNEL_SIZE)
-    criterion = nn.BCEWithLogitsLoss()
+    model = Net(kernel_size=KERNEL_SIZE).to(device)
+    criterion = nn.MSELoss()
     optimizer = optimizers.Adam(model.parameters(), lr=0.0002)
 
     # モデルの学習
