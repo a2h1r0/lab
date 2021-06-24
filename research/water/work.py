71fa9a4b138ccd5964946f5b9d6e9b0fba36940d
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-24 22:15:30 +0900

    テストデータ作成部分の実装

--- research/water/work.py
+++ research/water/work.py
@@ -5,17 +5,20 @@ import torch.nn as nn
 import torch.optim as optimizers
 from model import Net
 import matplotlib.pyplot as plt
+import random
 import os
 os.chdir(os.path.dirname(__file__))
 
 
 SOUND_DIR = './sounds/'
-SOUND_FILE = 'shampoo_1.mp3'  # 音源
+TRAIN_FILES = ['shampoo_1.mp3', 'shampoo_2.mp3']  # 学習用音源
+TEST_FILE = 'shampoo_3.mp3'  # テスト用音源
 
 EPOCH_NUM = 100  # 学習サイクル数
 KERNEL_SIZE = 7  # カーネルサイズ（奇数のみ）
-WINDOW_SIZE = 100000
-STEP = 10000
+WINDOW_SIZE = 100000  # 1サンプルのサイズ
+STEP = 1000  # 学習データのステップ幅
+TEST_DATA_NUM = 100  # テストデータ数
 
 
 def make_train_data():
@@ -23,21 +26,46 @@ def make_train_data():
     学習データの作成
     """
 
+    train_data, train_labels = [], []
+
+    for filename in TRAIN_FILES:
+        # 音源の読み出し
+        sound = AudioSegment.from_file(SOUND_DIR + filename, 'mp3')
+
+        # データの整形
+        data = np.array(sound.get_array_of_samples())
+        labels = np.linspace(0, 100, len(data))
+
+        for index in range(0, len(data) - WINDOW_SIZE + 1, STEP):
+            start = index
+            end = start + WINDOW_SIZE - 1
+            train_data.append(data[start:end + 1])
+            train_labels.append(labels[end])
+
+    return train_data, train_labels
+
+
+def make_test_data():
+    """
+    テストデータの作成
+    """
+
+    test_data, test_labels = [], []
+
     # 音源の読み出し
-    sound = AudioSegment.from_file(SOUND_DIR + SOUND_FILE, 'mp3')
+    sound = AudioSegment.from_file(SOUND_DIR + TEST_FILE, 'mp3')
 
     # データの整形
     data = np.array(sound.get_array_of_samples())
     labels = np.linspace(0, 100, len(data))
 
-    train_data, train_labels = [], []
-    for index in range(0, len(data)-WINDOW_SIZE+1, STEP):
-        start = index
-        end = index + WINDOW_SIZE - 1
-        train_data.append(data[start:end+1])
-        train_labels.append(labels[end])
+    for index in range(TEST_DATA_NUM):
+        start = random.randint(0, len(data) - WINDOW_SIZE)
+        end = start + WINDOW_SIZE - 1
+        test_data.append(data[start:end + 1])
+        test_labels.append(labels[end])
 
-    return train_data, train_labels
+    return test_data, test_labels
 
 
 def main():
@@ -77,7 +105,19 @@ def main():
         """
 
         model.eval()
-        # テスト処理
+        with torch.no_grad():
+            # テストデータの作成
+            test_data, test_labels = make_test_data()
+            print(1)
+
+        #     inputs, labels = data
+        #     outputs = net(inputs)
+        #     _, predicted = torch.max(outputs, 1)
+        #     total += len(outputs)
+        #     correct += (predicted == labels).sum().item()
+
+        # print(
+        #     f'correct: {correct}, accuracy: {correct} / {total} = {correct / total}')
 
     # モデルの構築
     model = Net(kernel_size=KERNEL_SIZE).to(device)
