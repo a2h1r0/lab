4503a81b917a88c552ee18654457059fcc5cfcf6
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-06-24 13:52:22 +0900

    Update flex_display.ino

--- research/pulse/generate_heart_rate/flex_display/flex_display.ino
+++ research/pulse/generate_heart_rate/flex_display/flex_display.ino
@@ -1,28 +1,31 @@
-// 実行時間
-const int PROCESS_TIME = 40;
-
+// シリアルスピード
+#define SPEED 115200
 // 使用ピン
-const int PWM_1 = 5;
-const int PWM_2 = 6;
+#define PWM_1 5
+#define PWM_2 6
+// 電圧
+#define PWM_HIGH 2
+#define PWM_LOW 0
+// 閾値
+#define THRESHOLD_PULSE 650
+#define THRESHOLD_TIME 300000
+
 // 黒の電圧値 (V)
-const int BLACK_PWM_1 = 5;
-const int BLACK_PWM_2 = 0;
+const int BLACK_PWM_1 = PWM_HIGH;
+const int BLACK_PWM_2 = PWM_LOW;
 // 白の電圧値 (V)
-const int WHITE_PWM_1 = 0;
-const int WHITE_PWM_2 = 5;
+const int WHITE_PWM_1 = PWM_LOW;
+const int WHITE_PWM_2 = PWM_HIGH;
 // 色データ長
 const int L_COLORS = 2;
 // 色データ
 const int COLORS_PWM_1[L_COLORS] = {WHITE_PWM_1, BLACK_PWM_1};
 const int COLORS_PWM_2[L_COLORS] = {WHITE_PWM_2, BLACK_PWM_2};
-// 目標心拍数受け取り用配列
-char heart_rate_data[4] = {'\0'};
-int i = 0;
-unsigned long drew_time;
-int last_heart_rate;
-unsigned long last_peak = 0;
-const int THRESHOLD_PULSE = 650;
-const unsigned long THRESHOLD_TIME = 300000;
+
+int heart_rate;              // 心拍数
+unsigned long last_peak = 0; // 1つ前のピーク検出時刻
+unsigned long drew_time;     // ディスプレイ描画時刻
+int i = 0;                   // Colorsカウンタ
 
 /**
  * 初期化
@@ -31,50 +34,47 @@ void setup()
 {
     pinMode(PWM_1, OUTPUT);
     pinMode(PWM_2, OUTPUT);
-    Serial.begin(9600);
+    Serial.begin(SPEED);
 }
 
 /**
- * 心拍数の取得
- * 
- * @return 心拍数
+ * 脈波の取得
  */
-int get_pulse()
+void get_pulse()
 {
     int pulse = analogRead(A0);
-    int heart_rate = get_heart_rate(pulse);
 
-    return heart_rate;
+    // 心拍数の更新
+    update_heart_rate(pulse);
 }
 
 /**
- * 心拍数の取得
+ * 心拍数の更新
  * 
- * @return 心拍数
+ * @param pulse 脈波値
  */
-int get_heart_rate(int pulse)
+void update_heart_rate(int pulse)
 {
     // 現在時刻の取得
     unsigned long now = micros();
 
+    // ピーク検出
     if (now > last_peak + THRESHOLD_TIME && pulse > THRESHOLD_PULSE)
     {
         // ピーク間隔を計算
-        unsigned long interval = last_peak - now;
-        // ピーク検出，心拍数を計算
+        unsigned long interval = now - last_peak;
+        // 心拍数の更新
+        heart_rate = (60 * 1000000) / interval;
         last_peak = now;
     }
-
-    return 60;
 }
 
 /**
  * 点灯時間の計算
  * 
- * @param heart_rate 心拍数
  * @return 点灯時間
  */
-unsigned long get_lighting_time(int heart_rate)
+unsigned long get_lighting_time()
 {
     // 点灯時間の計算
     float time = (float)60 / (L_COLORS * heart_rate);
@@ -95,6 +95,7 @@ void draw_display()
 
     // 描画した時刻を保存
     drew_time = micros();
+    Serial.println(heart_rate);
 
     // Colorsを更新
     if (i == L_COLORS - 1)
@@ -112,10 +113,11 @@ void draw_display()
  */
 void loop()
 {
-    // 心拍数の取得
-    int heart_rate = get_heart_rate();
+    // 脈波の取得
+    get_pulse();
+
     // 点灯時間の計算
-    unsigned long lighting_time = get_lighting_time(heart_rate);
+    unsigned long lighting_time = get_lighting_time();
 
     // 現在時刻の取得
     unsigned long now = micros();
