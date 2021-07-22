b9a65ba214c98f54bb73c698b3d255dac9c35df2
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-07-22 18:48:52 +0900

    Update work.py

--- research/ABC2021BentoChallenge/work/work.py
+++ research/ABC2021BentoChallenge/work/work.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optimizers
@@ -9,11 +10,13 @@ from model import Net
 from preprocess import make_feature
 from label_determination import majority_vote_sigmoid
 import matplotlib.pyplot as plt
+from natsort import natsorted
 import csv
 import glob
 import re
 import datetime
 import random
+import copy
 import sys
 import os
 os.chdir(os.path.dirname(__file__))
@@ -21,9 +24,8 @@ os.chdir(os.path.dirname(__file__))
 
 DATA_DIR = '../dataset/train/acceleration/1_13/'
 
-# USE_MARKERS = ['right_shoulder', 'right_elbow', 'right_wrist',
-#                'left_shoulder', 'left_elbow', 'left_wrist']
-USE_MARKERS = ['right_shoulder', 'right_elbow']
+USE_MARKERS = ['right_shoulder', 'right_elbow', 'right_wrist',
+               'left_shoulder', 'left_elbow', 'left_wrist']
 
 NUM_CLASSES = 10  # クラス数
 BAGGING_NUM = 10  # モデル入れ替え試行回数
@@ -37,11 +39,12 @@ def make_train_data():
     学習データの作成
 
     Returns:
-        train_data (array): 学習データ
-        train_labels (array): 学習データラベル
+        array: 学習データ
+        array: 学習データラベル
+        array: 学習データファイル
     """
 
-    train_data, train_labels = [], []
+    train_data, train_labels, train_files = [], [], []
     files = glob.glob(DATA_DIR + '/subject_[' + ''.join(TRAIN_SUBJECTS) + ']*.csv')
     for filename in files:
         with open(filename) as f:
@@ -55,8 +58,9 @@ def make_train_data():
         activity = re.findall(r'activity_\d+', filename)[0]
         label = int(activity.split('_')[1])
         train_labels.append(multi_label_binarizer(label))
+        train_files.append(filename.split('\\')[-1])
 
-    return train_data, train_labels
+    return train_data, train_labels, train_files
 
 
 def make_test_data():
@@ -64,9 +68,9 @@ def make_test_data():
     テストデータの作成
 
     Returns:
-        test_data (array): テストデータ
-        test_labels (array): テストデータラベル
-        answer_labels (array): テストデータ生ラベル
+        array: テストデータ
+        array: テストデータラベル
+        array: テストデータ生ラベル
     """
 
     test_data, test_labels, answer_labels = [], [], []
@@ -88,37 +92,32 @@ def make_test_data():
     return test_data, test_labels, answer_labels
 
 
-def get_random_data(train_data_all, train_labels_all):
+def get_random_data(train_data_all, train_labels_all, train_files_all):
     """
     ランダムデータの作成
 
     Args:
         train_data_all (array): 学習データ全件
         train_labels_all (array): 学習データラベル全件
+        train_files_all (array): 学習データラベル全件
     Returns:
-        train_data (array): 学習データ
-        train_labels (array): 学習データラベル
+        array: 学習データ
+        array: 学習データラベル
+        array: 学習データファイル
     """
 
+    train_data = copy.deepcopy(train_data_all)
+    train_labels = copy.deepcopy(train_labels_all)
     sample_num = len(train_data_all)
     delete_index = random.sample(range(sample_num), sample_num // 10)
-    test_data, test_labels, answer_labels = [], [], []
-    files = glob.glob(DATA_DIR + '/subject_' + TEST_SUBJECT + '*.csv')
-    for filename in files:
-        with open(filename) as f:
-            reader = csv.reader(f)
-            next(reader)
-            raw_data = [row for row in reader]
-            feature_data = make_feature(raw_data, USE_MARKERS)
-            if len(feature_data[0]) < 5:
-                continue
-        test_data.append(torch.tensor(feature_data, dtype=torch.float, device=device))
-        activity = re.findall(r'activity_\d+', filename)[0]
-        label = int(activity.split('_')[1])
-        test_labels.append(multi_label_binarizer(label))
-        answer_labels.append(label)
 
-    return test_data, test_labels, answer_labels
+    for index in sorted(delete_index, reverse=True):
+        del train_data[index]
+        del train_labels[index]
+
+    train_files = [train_file for index, train_file in enumerate(train_files_all) if index not in delete_index]
+
+    return train_data, train_labels, train_files
 
 
 def get_marker_data(marker_index, data):
@@ -240,12 +239,12 @@ def main():
     optimizer = optimizers.Adam(model.parameters())
 
     # データの読み込み
-    train_data_all, train_labels_all = make_train_data()
+    train_data_all, train_labels_all, train_files_all = make_train_data()
     test_data_full_markers, test_labels, answer_labels = make_test_data()
 
     for num in range(BAGGING_NUM):
         # データの切り捨て
-        train_data_full_markers, train_labels = get_random_data(train_data_all, train_labels_all)
+        train_data_full_markers, train_labels, train_files = get_random_data(train_data_all, train_labels_all, train_files_all)
 
         loss_all = []
         predictions = []
@@ -259,25 +258,28 @@ def main():
             # モデルのテスト
             test()
 
-        # 部位ごとの精度の計算
+        # 部位ごとの結果の保存
+        data_dir = '../data/' + now + '/' + str(num+1) + '/'
+        if os.path.exists(data_dir) == False:
+            os.makedirs(data_dir)
+        train_file_list = data_dir + 'train_file_list.txt'
+        with open(train_file_list, 'w', newline='') as f:
+            for filename in natsorted(train_files):
+                f.write(filename + '\n')
+
         for marker, prediction_single in zip(USE_MARKERS, predictions):
-            print('---' + marker + '---')
-            for answer, prediction in zip(answer_labels, prediction_single):
-                print('Answer: ' + str(answer) + ' / Prediction: ' + str(sigmoid_to_label(prediction)))
+            prediction_labels_single = [sigmoid_to_label(prediction) for prediction in prediction_single]
+            report_df = pd.DataFrame(classification_report(answer_labels, prediction_labels_single, output_dict=True))
+            report_df.to_csv(data_dir + 'report_' + marker + '.csv')
 
         # 予測ラベルの決定
         prediction_labels = label_determination(predictions)
 
-        # 結果の表示
-        for answer, prediction in zip(answer_labels, prediction_labels):
-            print('Answer: ' + str(answer) + ' / Prediction: ' + str(prediction))
-        print(classification_report(answer_labels, prediction_labels))
-
-        # Lossの保存
-        loss_dir = '../data/' + now
-        if os.path.exists(loss_dir) == False:
-            os.makedirs(loss_dir)
-        loss_file = loss_dir + '/loss_train' + ''.join(TRAIN_SUBJECTS) + '_test' + TEST_SUBJECT + '.csv'
+        # 全体の結果の保存
+        report_df = pd.DataFrame(classification_report(answer_labels, prediction_labels, output_dict=True))
+        report_df.to_csv(data_dir + 'report_all.csv')
+        print(report_df)
+        loss_file = data_dir + 'loss_train' + ''.join(TRAIN_SUBJECTS) + '_test' + TEST_SUBJECT + '.csv'
         with open(loss_file, 'w', newline='') as f:
             loss_writer = csv.writer(f)
             loss_writer.writerow(['Epoch'] + USE_MARKERS)
@@ -286,13 +288,13 @@ def main():
                 loss_writer.writerow([epoch + 1] + list(loss))
 
         # 結果の描画
-        figures_dir = '../figures/' + now
+        figures_dir = '../figures/' + now + str(num+1) + '/'
         if os.path.exists(figures_dir) == False:
             os.makedirs(figures_dir)
         print('\n結果を描画します．．．')
         plt.figure()
         sns.heatmap(confusion_matrix(answer_labels, prediction_labels))
-        plt.savefig(figures_dir + '/result_train' + ''.join(TRAIN_SUBJECTS) + '_test' + TEST_SUBJECT + '.png', bbox_inches='tight', pad_inches=0)
+        plt.savefig(figures_dir + 'result_train' + ''.join(TRAIN_SUBJECTS) + '_test' + TEST_SUBJECT + '.png', bbox_inches='tight', pad_inches=0)
 
         # Lossの描画
         plt.figure(figsize=(16, 9))
@@ -302,7 +304,7 @@ def main():
         plt.ylabel('Loss', fontsize=26)
         plt.legend(fontsize=26, loc='upper right')
         plt.tick_params(labelsize=26)
-        plt.savefig(figures_dir + '/loss_train' + ''.join(TRAIN_SUBJECTS) + '_test' + TEST_SUBJECT + '.png', bbox_inches='tight', pad_inches=0)
+        plt.savefig(figures_dir + 'loss_train' + ''.join(TRAIN_SUBJECTS) + '_test' + TEST_SUBJECT + '.png', bbox_inches='tight', pad_inches=0)
 
 
 if __name__ == '__main__':
