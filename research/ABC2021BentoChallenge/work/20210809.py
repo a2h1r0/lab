896a2952bff848da476aa6643247f9fc8503cadb
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-08-09 15:03:56 +0900

    update

--- research/ABC2021BentoChallenge/work/20210809.py
+++ research/ABC2021BentoChallenge/work/20210809.py
@@ -6,12 +6,11 @@ import torch.optim as optimizers
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
-from model import Net
+from model import NetAll
 from preprocess import make_feature
 from label_determination import majority_vote_sigmoid
 import matplotlib.pyplot as plt
 from natsort import natsorted
-import math
 import csv
 import glob
 import re
@@ -28,9 +27,7 @@ DATA_DIR = '../dataset/train/speed/1_13/'
 USE_MARKERS = ['right_shoulder', 'right_elbow', 'right_wrist',
                'left_shoulder', 'left_elbow', 'left_wrist']
 
-FEATURE_SIZE = 21  # 特徴量次元数
-NUM_CLASSES_MACRO = 5  # マクロクラス数
-NUM_CLASSES_MICRO = 2  # マイクロクラス数
+NUM_CLASSES = 10  # クラス数
 EPOCH_NUM = 5000  # 学習サイクル数
 HIDDEN_SIZE = 24  # 隠れ層数
 LABEL_THRESHOLD = 0.0  # ラベルを有効にする閾値
@@ -43,9 +40,10 @@ def make_train_data():
     Returns:
         array: 学習データ
         array: 学習データラベル
+        array: 学習データファイル
     """
 
-    train_data, train_labels_macro, train_labels_micro = [], [], []
+    train_data, train_labels, train_files = [], [], []
     files = glob.glob(DATA_DIR + '/subject_[' + ''.join(TRAIN_SUBJECTS) + ']*.csv')
     for filename in files:
         with open(filename) as f:
@@ -58,10 +56,10 @@ def make_train_data():
         train_data.append(torch.tensor(feature_data, dtype=torch.float, device=device))
         activity = re.findall(r'activity_\d+', filename)[0]
         label = int(activity.split('_')[1])
-        train_labels_macro.append(multi_label_binarizer_macro(label))
-        train_labels_micro.append(multi_label_binarizer_micro(label))
+        train_labels.append(multi_label_binarizer(label))
+        train_files.append(filename.split('\\')[-1])
 
-    return train_data, train_labels_macro, train_labels_micro
+    return train_data, train_labels, train_files
 
 
 def make_test_data():
@@ -74,7 +72,7 @@ def make_test_data():
         array: テストデータ生ラベル
     """
 
-    test_data, answer_labels, answer_labels_macro, answer_labels_micro = [], [], [], []
+    test_data, test_labels, answer_labels = [], [], []
     files = glob.glob(DATA_DIR + '/subject_' + TEST_SUBJECT + '*.csv')
     for filename in files:
         with open(filename) as f:
@@ -87,11 +85,38 @@ def make_test_data():
         test_data.append(torch.tensor(feature_data, dtype=torch.float, device=device))
         activity = re.findall(r'activity_\d+', filename)[0]
         label = int(activity.split('_')[1])
+        test_labels.append(multi_label_binarizer(label))
         answer_labels.append(label)
-        answer_labels_macro.append(multi_label_binarizer_macro(label))
-        answer_labels_micro.append(multi_label_binarizer_micro(label))
 
-    return test_data, answer_labels, answer_labels_macro, answer_labels_micro
+    return test_data, test_labels, answer_labels
+
+
+def get_random_data(train_data_all, train_labels_all, train_files_all):
+    """
+    ランダムデータの作成
+
+    Args:
+        train_data_all (array): 学習データ全件
+        train_labels_all (array): 学習データラベル全件
+        train_files_all (array): 学習データラベル全件
+    Returns:
+        array: 学習データ
+        array: 学習データラベル
+        array: 学習データファイル
+    """
+
+    train_data = copy.deepcopy(train_data_all)
+    train_labels = copy.deepcopy(train_labels_all)
+    sample_num = len(train_data_all)
+    delete_index = random.sample(range(sample_num), sample_num // 10)
+
+    for index in sorted(delete_index, reverse=True):
+        del train_data[index]
+        del train_labels[index]
+
+    train_files = [train_file for index, train_file in enumerate(train_files_all) if index not in delete_index]
+
+    return train_data, train_labels, train_files
 
 
 def get_marker_data(marker_index, data):
@@ -108,9 +133,9 @@ def get_marker_data(marker_index, data):
     return [row[marker_index] for row in data]
 
 
-def multi_label_binarizer_macro(label):
+def multi_label_binarizer(label):
     """
-    ラベルの5クラスワンホット化
+    ラベルのワンホット化
 
     Args:
         label (int): ラベル
@@ -118,49 +143,10 @@ def multi_label_binarizer_macro(label):
         array: ワンホットラベル
     """
 
-    y = [0 for i in range(5)]
-    y[math.ceil(label/2) - 1] = 1
+    y = [0 for i in range(10)]
+    y[label - 1] = 1
 
     return y
-
-
-def multi_label_binarizer_micro(label):
-    """
-    ラベルの2クラスワンホット化
-
-    Args:
-        label (int): ラベル
-    Returns:
-        array: ワンホットラベル
-    """
-
-    y = [0 for i in range(2)]
-    y[~(label % 2)] = 1
-
-    return y
-
-
-def get_10_prediction(prediction_macro, prediction_micro):
-    """
-    マクロ予測とマイクロ予測の合算
-
-    Args:
-        prediction_macro (array): マクロ予測
-        prediction_micro (array): マイクロ予測
-    Returns:
-        array: 10ラベル予測
-    """
-
-    predictions = []
-    for macro_labels, micro_labels in zip(prediction_macro, prediction_micro):
-        prediction = []
-        for macro in macro_labels:
-            for micro in micro_labels:
-                prediction.append(macro * micro)
-
-        predictions.append(prediction)
-
-    return predictions
 
 
 def sigmoid_to_label(prediction):
@@ -183,37 +169,26 @@ def main():
         """
 
         # データの作成
-        train_data = get_marker_data(marker, train_data_all)
+        train_data = get_marker_data(marker, train_data_full_markers)
         train_data_length = [len(data) for data in train_data]
 
-        model.Macro.train()
-        model.Micro.train()
+        model.train()
         print('\n***** 学習開始 *****')
 
         for epoch in range(EPOCH_NUM):
             # パディング処理
             inputs = torch.nn.utils.rnn.pad_sequence(train_data, batch_first=True).permute(0, 2, 1).to(device)
+            labels = torch.tensor(train_labels, dtype=torch.float, device=device)
 
-            # macro識別
-            labels_macro = torch.tensor(train_labels_macro, dtype=torch.float, device=device)
-            optimizer_macro.zero_grad()
-            outputs_macro = model.Macro(inputs, train_data_length)
-            loss_macro = criterion_macro(outputs_macro, labels_macro)
-            loss_macro.backward()
-            optimizer_macro.step()
+            optimizer.zero_grad()
+            outputs = model(inputs, train_data_length)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-            # micro識別
-            labels_micro = torch.tensor(train_labels_micro, dtype=torch.float, device=device)
-            optimizer_micro.zero_grad()
-            outputs_micro = model.Micro(inputs, train_data_length)
-            loss_micro = criterion_micro(outputs_micro, labels_micro)
-            loss_micro.backward()
-            optimizer_micro.step()
-
-            loss_macro_all[-1].append(loss_macro.item())
-            loss_micro_all[-1].append(loss_micro.item())
+            loss_all[-1].append(loss.item())
             if (epoch + 1) % 10 == 0:
-                print('Epoch: {} / Loss macro: {:.3f} / Loss micro: {:.3f}'.format(epoch + 1, loss_macro.item(), loss_micro.item()))
+                print('Epoch: {} / Loss: {:.3f}'.format(epoch + 1, loss.item()))
 
         print('\n----- 終了 -----\n')
 
@@ -223,28 +198,21 @@ def main():
         """
 
         # データの作成
-        test_data = get_marker_data(marker, test_data_all)
+        test_data = get_marker_data(marker, test_data_full_markers)
         test_data_length = [len(data) for data in test_data]
 
-        model.Macro.eval()
-        model.Micro.eval()
+        model.eval()
         print('\n***** テスト *****')
 
         with torch.no_grad():
             # パディング処理
             inputs = torch.nn.utils.rnn.pad_sequence(test_data, batch_first=True).permute(0, 2, 1).to(device)
+            labels = torch.tensor(test_labels, dtype=torch.float, device=device)
 
-            # macro識別
-            outputs_macro = model.Macro(inputs, test_data_length)
-            prediction_macro = torch.sigmoid(outputs_macro)
-            prediction_macro = prediction_macro.to('cpu').detach().numpy().copy()
-
-            # micro識別
-            outputs_micro = model.Micro(inputs, test_data_length)
-            prediction_micro = torch.sigmoid(outputs_micro)
-            prediction_micro = prediction_micro.to('cpu').detach().numpy().copy()
-
-            predictions.append(get_10_prediction(prediction_macro, prediction_micro))
+            outputs = model(inputs, test_data_length)
+            # 予測結果をSigmoidに通す
+            prediction = torch.sigmoid(outputs)
+            predictions.append(prediction.to('cpu').detach().numpy().copy())
 
     def label_determination(predictions):
         """
@@ -264,27 +232,25 @@ def main():
         return labels
 
     # モデルの構築
-    model = Net(input_size=FEATURE_SIZE, hidden_size=HIDDEN_SIZE,
-                out_features_macro=NUM_CLASSES_MACRO, out_features_micro=NUM_CLASSES_MICRO, device=device)
-    pos_weight_macro = torch.ones([NUM_CLASSES_MACRO], device=device)
-    criterion_macro = nn.BCEWithLogitsLoss(pos_weight=pos_weight_macro)
-    optimizer_macro = optimizers.Adam(model.Macro.parameters())
-    pos_weight_micro = torch.ones([NUM_CLASSES_MICRO], device=device)
-    criterion_micro = nn.BCEWithLogitsLoss(pos_weight=pos_weight_micro)
-    optimizer_micro = optimizers.Adam(model.Micro.parameters())
+    model = NetAll(input_size=21, hidden_size=HIDDEN_SIZE, out_features=NUM_CLASSES).to(device)
+    pos_weight = torch.ones([NUM_CLASSES], device=device)
+    criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+    optimizer = optimizers.Adam(model.parameters())
 
     # データの読み込み
-    train_data_all, train_labels_macro, train_labels_micro = make_train_data()
-    test_data_all, answer_labels, answer_labels_macro, answer_labels_micro = make_test_data()
+    train_data_all, train_labels_all, train_files_all = make_train_data()
+    test_data_full_markers, test_labels, answer_labels = make_test_data()
 
-    loss_macro_all, loss_micro_all = [], []
+    # データの切り捨て
+    train_data_full_markers, train_labels, train_files = get_random_data(train_data_all, train_labels_all, train_files_all)
+
+    loss_all = []
     predictions = []
     for marker in range(len(USE_MARKERS)):
         print('\n!!!!! ' + USE_MARKERS[marker] + ' !!!!!')
 
         # モデルの学習
-        loss_macro_all.append([])
-        loss_micro_all.append([])
+        loss_all.append([])
         train()
 
         # モデルのテスト
@@ -296,6 +262,11 @@ def main():
     data_dir = '../data/' + now + '/'
     if os.path.exists(data_dir) == False:
         os.makedirs(data_dir)
+    train_file_save = data_dir + 'train_files_' + subjects + '.csv'
+    with open(train_file_save, 'w', newline='') as f:
+        train_files_writer = csv.writer(f)
+        train_files_writer.writerows([[filename] for filename in natsorted(train_files)])
+
     for marker, prediction_single in zip(USE_MARKERS, predictions):
         prediction_labels_single = [sigmoid_to_label(prediction) for prediction in prediction_single]
         report_df = pd.DataFrame(classification_report(answer_labels, prediction_labels_single, output_dict=True))
@@ -308,18 +279,13 @@ def main():
     report_df = pd.DataFrame(classification_report(answer_labels, prediction_labels, output_dict=True))
     report_df.to_csv(data_dir + 'report_all_' + subjects + '.csv')
     print(report_df)
-    loss_macro_file = data_dir + 'loss_macro_' + subjects + '.csv'
-    with open(loss_macro_file, 'w', newline='') as f:
-        loss_macro_writer = csv.writer(f)
-        loss_macro_writer.writerow(['Epoch'] + USE_MARKERS)
-        for epoch, loss in enumerate(np.array(loss_macro_all).T):
-            loss_macro_writer.writerow([epoch + 1] + list(loss))
-    loss_micro_file = data_dir + 'loss_micro_' + subjects + '.csv'
-    with open(loss_micro_file, 'w', newline='') as f:
-        loss_micro_writer = csv.writer(f)
-        loss_micro_writer.writerow(['Epoch'] + USE_MARKERS)
-        for epoch, loss in enumerate(np.array(loss_micro_all).T):
-            loss_micro_writer.writerow([epoch + 1] + list(loss))
+    loss_file = data_dir + 'loss_' + subjects + '.csv'
+    with open(loss_file, 'w', newline='') as f:
+        loss_writer = csv.writer(f)
+        loss_writer.writerow(['Epoch'] + USE_MARKERS)
+
+        for epoch, loss in enumerate(np.array(loss_all).T):
+            loss_writer.writerow([epoch + 1] + list(loss))
 
     # 結果の描画
     figures_dir = '../figures/' + now + '/'
@@ -332,12 +298,11 @@ def main():
 
     # Lossの描画
     plt.figure(figsize=(16, 9))
-    for marker, loss_macro, loss_micro in zip(USE_MARKERS, loss_macro_all, loss_micro_all):
-        plt.plot(range(1, EPOCH_NUM + 1), loss_macro, linestyle='solid', label=marker)
-        plt.plot(range(1, EPOCH_NUM + 1), loss_micro, linestyle='dashed', label=marker)
+    for marker, loss in zip(USE_MARKERS, loss_all):
+        plt.plot(range(1, EPOCH_NUM + 1), loss, label=marker)
     plt.xlabel('Epoch', fontsize=26)
     plt.ylabel('Loss', fontsize=26)
-    plt.legend()
+    plt.legend(fontsize=26, loc='upper right')
     plt.tick_params(labelsize=26)
     plt.savefig(figures_dir + 'loss_' + subjects + '.png', bbox_inches='tight', pad_inches=0)
 
