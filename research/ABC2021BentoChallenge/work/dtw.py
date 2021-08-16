6734f92d62722d022399fcb739b655598452f289
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-08-16 20:12:07 +0900

    Create dtw.py

---
+++ research/ABC2021BentoChallenge/work/dtw.py
@@ -0,0 +1,161 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+from fastdtw import fastdtw
+from preprocess import make_raw
+from natsort import natsorted
+import csv
+import glob
+import re
+import datetime
+import sys
+import os
+os.chdir(os.path.dirname(__file__))
+
+
+DATA_DIR = '../dataset/train/autocorrelation/'
+
+USE_MARKERS = ['right_shoulder', 'right_elbow', 'right_wrist',
+               'left_shoulder', 'left_elbow', 'left_wrist']
+
+
+def make_train_data():
+    """
+    学習データの作成
+
+    Returns:
+        array: 学習データ
+        array: 学習データラベル
+        array: 学習データファイル
+    """
+
+    train_data, labels = [], []
+    files = glob.glob(DATA_DIR + '/subject_[' + ''.join(TRAIN_SUBJECTS) + ']*.csv')
+    for filename in files:
+        with open(filename) as f:
+            reader = csv.reader(f)
+            next(reader)
+            raw_data = [row for row in reader]
+            feature_data = make_raw(raw_data, USE_MARKERS)
+        wave_data = []
+        for single_data in feature_data:
+            wave = []
+            for data in single_data:
+                wave.append(np.sum(data))
+            wave_data.append(wave)
+        train_data.append(wave_data)
+        activity = re.findall(r'activity_\d+', filename)[0]
+        label = int(activity.split('_')[1])
+        labels.append(label)
+
+    return train_data, labels
+
+
+def make_test_data():
+    """
+    テストデータの作成
+
+    Returns:
+        array: テストデータ
+        array: テストデータラベル
+        array: テストデータ生ラベル
+    """
+
+    test_data, answer_labels = [], []
+    files = glob.glob(DATA_DIR + '/subject_' + TEST_SUBJECT + '*.csv')
+    for filename in files:
+        with open(filename) as f:
+            reader = csv.reader(f)
+            next(reader)
+            raw_data = [row for row in reader]
+            feature_data = make_raw(raw_data, USE_MARKERS)
+        wave_data = []
+        for single_data in feature_data:
+            wave = []
+            for data in single_data:
+                wave.append(np.sum(data))
+            wave_data.append(wave)
+        test_data.append(wave_data)
+        activity = re.findall(r'activity_\d+', filename)[0]
+        label = int(activity.split('_')[1])
+        answer_labels.append(label)
+
+    return test_data, answer_labels
+
+
+def get_marker_data(marker_index, data):
+    """
+    部位ごとのデータの取得
+
+    Args:
+        marker_index (int): 使用する部位のインデックス
+        data (array): データ
+    Returns:
+        array: 部位ごとのデータ
+    """
+
+    return [row[marker_index] for row in data]
+
+
+def main():
+    # データの読み込み
+    train_data_all, labels = make_train_data()
+    test_data_all, answer_labels = make_test_data()
+
+    predicrions = []
+    for test_data in test_data_all:
+        prediction_labels = []
+        for marker in range(len(test_data)):
+            test_data_single = test_data[marker]
+            train_data = get_marker_data(marker, train_data_all)
+            print(train_data_all)
+
+    sys.exit()
+    # データの作成
+    test_data = get_marker_data(marker, test_data_all)
+
+    loss_all = []
+    predictions = []
+    for marker in range(len(USE_MARKERS)):
+        print('\n!!!!! ' + USE_MARKERS[marker] + ' !!!!!')
+
+        # モデルの学習
+        loss_all.append([])
+        train()
+
+        # モデルのテスト
+        test()
+
+    subjects = 'train' + ''.join(TRAIN_SUBJECTS) + '_test' + TEST_SUBJECT
+
+    # 結果の保存
+    data_dir = '../data/LSTM1/' + now + '/'
+    if os.path.exists(data_dir) == False:
+        os.makedirs(data_dir)
+    report_df = pd.DataFrame(classification_report(answer_labels, prediction_labels, output_dict=True))
+    report_df.to_csv(data_dir + 'report_all_' + subjects + '.csv')
+    print(report_df)
+
+
+if __name__ == '__main__':
+    now = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+
+    TRAIN_SUBJECTS = ['1']
+    TEST_SUBJECT = '2'
+    main()
+    TEST_SUBJECT = '3'
+    main()
+
+    TRAIN_SUBJECTS = ['2']
+    TEST_SUBJECT = '1'
+    main()
+    TEST_SUBJECT = '3'
+    main()
+
+    TRAIN_SUBJECTS = ['3']
+    TEST_SUBJECT = '1'
+    main()
+    TEST_SUBJECT = '2'
+    main()
