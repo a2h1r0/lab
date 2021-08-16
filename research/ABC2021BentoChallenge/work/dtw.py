f52e6ab980c4a6213eb7551f9be4f1dc09325a3c
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-08-16 21:55:40 +0900

    Update dtw.py

--- research/ABC2021BentoChallenge/work/dtw.py
+++ research/ABC2021BentoChallenge/work/dtw.py
@@ -51,8 +51,6 @@ def make_train_data():
         activity = re.findall(r'activity_\d+', filename)[0]
         label = int(activity.split('_')[1])
         labels.append(label)
-        if len(train_data) > 2:
-            break
 
     return train_data, labels
 
@@ -85,8 +83,6 @@ def make_test_data():
         activity = re.findall(r'activity_\d+', filename)[0]
         label = int(activity.split('_')[1])
         answer_labels.append(label)
-        if len(test_data) > 2:
-            break
 
     return test_data, answer_labels
 
@@ -110,9 +106,11 @@ def main():
     train_data_all, labels = make_train_data()
     test_data_all, answer_labels = make_test_data()
 
-    predictions = []
-    for test_data in test_data_all:
-        prediction_labels, prediction_distances = [], []
+    prediction_labels = []
+    for index, test_data in enumerate(test_data_all):
+        print('残り{:d}件．．．'.format(len(test_data_all) - index))
+
+        predictions, prediction_distances = [], []
         for marker in range(len(test_data)):
             test_data_single = test_data[marker]
             train_data_singles = get_marker_data(marker, train_data_all)
@@ -122,38 +120,22 @@ def main():
                 distances.append(distance)
 
             min_index = np.argmin(distances)
-            prediction_labels.append(labels[min_index])
+            predictions.append(labels[min_index])
             prediction_distances.append(distances[min_index])
 
-        majority_labels = statistics.multimode(prediction_labels)
+        majority_labels = statistics.multimode(predictions)
         if len(majority_labels) == 1:
             label = majority_labels[0]
         else:
             min_distance_index = np.argmin(prediction_distances)
-            label = prediction_labels[min_distance_index]
+            label = predictions[min_distance_index]
 
-        predictions.append(label)
-
-    sys.exit()
-    # データの作成
-    test_data = get_marker_data(marker, test_data_all)
-
-    loss_all = []
-    predictions = []
-    for marker in range(len(USE_MARKERS)):
-        print('\n!!!!! ' + USE_MARKERS[marker] + ' !!!!!')
-
-        # モデルの学習
-        loss_all.append([])
-        train()
-
-        # モデルのテスト
-        test()
+        prediction_labels.append(label)
 
     subjects = 'train' + ''.join(TRAIN_SUBJECTS) + '_test' + TEST_SUBJECT
 
     # 結果の保存
-    data_dir = '../data/LSTM1/' + now + '/'
+    data_dir = '../data/' + now + '/'
     if os.path.exists(data_dir) == False:
         os.makedirs(data_dir)
     report_df = pd.DataFrame(classification_report(answer_labels, prediction_labels, output_dict=True))
