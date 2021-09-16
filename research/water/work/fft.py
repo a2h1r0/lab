f182d3bdbb592153154c9c9108d7bf616b3ed244
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-09-16 22:36:58 +0900

    Update fft.py

--- research/water/work/fft.py
+++ research/water/work/fft.py
@@ -9,21 +9,22 @@ os.chdir(os.path.dirname(__file__))
 
 SOUND_DATA = '../sounds/temp/shampoo/1.mp3'
 
-END = 6  # 終了（秒）
+# END = 6  # 終了（秒）
 
 
-def fft(sound):
+def fft(sound, end):
     """
     FFT
 
     Args:
+        end (int): 表示終了秒数
         sound (:obj:`Sound`): 音オブジェクト
     Returns:
         array: 周波数
         array: パワースペクトル
     """
 
-    end = END * 1000
+    end *= 1000
     if end > len(sound.get_array_of_samples()):
         end = sound.get_array_of_samples()
     start = end - 1000
@@ -43,26 +44,33 @@ def fft(sound):
 
 
 def main():
-    # 音源の読み出し
-    sound = AudioSegment.from_file(SOUND_DATA, 'mp3')
-    freqs, power = fft(sound)
-
-    delete_index = np.where(freqs < 0)
-    freqs = np.delete(freqs, delete_index)
-    power = np.delete(power, delete_index)
-    max_power_index = np.argmax(power)
-
-    print('Max Power Frequency: {:f}Hz'.format(freqs[max_power_index]))
-
     plt.figure(figsize=(16, 9))
-    plt.plot(freqs, power)
-    plt.xlim(0, max(freqs))
-    plt.title(str(END) + '~' + str(END + 1) + ' [s]')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 500000000000)
     plt.xlabel('Frequency', fontsize=26)
     plt.ylabel('Power', fontsize=26)
     plt.tick_params(labelsize=26)
-    plt.savefig(str(END) + '.eps', bbox_inches='tight', pad_inches=0)
-    # plt.show()
+
+    for i in range(6):
+        end = i + 1
+
+        # 音源の読み出し
+        sound = AudioSegment.from_file(SOUND_DATA, 'mp3')
+        freqs, power = fft(sound, end)
+
+        delete_index = np.where(freqs < 0)
+        freqs = np.delete(freqs, delete_index)
+        power = np.delete(power, delete_index)
+        max_power_index = np.argmax(power)
+
+        print('Max Power Frequency: {:f}Hz'.format(freqs[max_power_index]))
+
+        plt.plot(freqs, power, label='{:d}~{:d} [s]'.format(end, end + 1))
+
+    plt.legend()
+    # plt.title(str(END) + '~' + str(END + 1) + ' [s]')
+    # plt.savefig(str(END) + '.eps', bbox_inches='tight', pad_inches=0)
+    plt.show()
 
 
 if __name__ == '__main__':
