e18427e2014d1622881aa1399b178ae1349e89c8
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-09-18 22:38:20 +0900

    Create ticwatch.ino

---
+++ research/pulse/work/ticwatch/ticwatch.ino
@@ -0,0 +1,62 @@
+// フィルム直置き，ウォッチ下4mmアクリル板
+
+// 目標心拍数
+const int HEART_RATE = 80;
+
+// 使用ピン
+const int PWM_1 = 5;
+const int PWM_2 = 6;
+// 電圧値 (V)
+const int PWM_HIGH = 2;
+const int PWM_LOW = 0;
+// 色データ長
+const int L_COLORS = 5;
+// 色データ
+const int COLORS_PWM_1[L_COLORS] = {
+    PWM_HIGH,
+    PWM_HIGH / 2,
+    PWM_LOW,
+    PWM_HIGH / 2,
+    PWM_HIGH,
+};
+const int COLORS_PWM_2[L_COLORS] = {
+    PWM_LOW,
+    PWM_HIGH / 2,
+    PWM_HIGH,
+    PWM_HIGH / 2,
+    PWM_LOW,
+};
+
+/**
+ * @fn
+ * 初期化
+ */
+void setup()
+{
+    pinMode(PWM_1, OUTPUT);
+    pinMode(PWM_2, OUTPUT);
+    Serial.begin(9600);
+}
+
+/**
+ * @fn
+ * メインループ
+ */
+void loop()
+{
+    // 点灯時間の計算
+    float lighting_time = (float)60 / (L_COLORS * HEART_RATE);
+    // ミリ秒変換
+    int delay_time = lighting_time * 1000;
+
+    // 描画
+    while (true)
+    {
+        for (int i = 0; i < L_COLORS; i++)
+        {
+            analogWrite(PWM_1, COLORS_PWM_1[i] * 51);
+            analogWrite(PWM_2, COLORS_PWM_2[i] * 51);
+            delay(delay_time);
+        }
+    }
+}
