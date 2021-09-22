c91a0edcdc6f24e5a2c8996def9ce5aa22adc513
Atsuhiro <atsuhiro.fujii.98@gmail.com>
2021-09-22 22:03:12 +0900

    Update work_spectrogram.py

--- research/water/work/work_spectrogram.py
+++ research/water/work/work_spectrogram.py
@@ -22,9 +22,9 @@ BOTTLE = 'shampoo'
 SOUND_DIR = '../sounds/temp/' + BOTTLE + '/'
 
 
-EPOCH_NUM = 300  # 学習サイクル数
+EPOCH_NUM = 200  # 学習サイクル数
 KERNEL_SIZE = 3  # カーネルサイズ（奇数のみ）
-BATCH_SIZE = 1500  # バッチサイズ
+BATCH_SIZE = 2000  # バッチサイズ
 WINDOW_SECOND = 0.5  # 1サンプルの秒数
 STEP = 1000  # スライド幅
 TEST_ONEFILE_DATA_NUM = 100  # 1ファイルごとのテストデータ数
