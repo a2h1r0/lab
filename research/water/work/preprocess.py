c51a457fe65d8abc0c7344a73850cdf5b0d745d1
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-09-27 22:55:01 +0900

    Update preprocess.py

--- research/water/work/preprocess.py
+++ research/water/work/preprocess.py
@@ -10,21 +10,26 @@ TEMP_DIR = '../sounds/temp/shampoo/'  # 保存先
 THRESHOLD = -55  # 無音検出閾値
 
 
-def preprocess():
+def preprocess(filename):
     """
     音源データ前処理（無音部分の削除）
+
+    Args:
+        filename (string): 処理する音源ファイル名
     """
 
     if os.path.exists(TEMP_DIR) == False:
         os.makedirs(TEMP_DIR)
 
-    files = glob.glob(SOUND_DIR + '*.mp3')
-    for filename in files:
-        data = AudioSegment.from_file(filename, 'mp3')
-        sounds = split_on_silence(data, min_silence_len=2000, silence_thresh=THRESHOLD)
-        sound = sounds[0].set_channels(1)
-        sound.export(TEMP_DIR + filename.split('\\')[1].split('.')[0] + '.mp3', format='mp3')
+    data = AudioSegment.from_file(filename, 'mp3')
+    sounds = split_on_silence(data, min_silence_len=2000, silence_thresh=THRESHOLD)
+    sound = sounds[0].set_channels(1)
+    sound.export(TEMP_DIR + filename.split('\\')[1].split('.')[0] + '.mp3', format='mp3')
 
 
 if __name__ == '__main__':
-    preprocess()
+    files = glob.glob(SOUND_DIR + '*.mp3')
+    for filename in files:
+        preprocess(filename)
+
+    # preprocess('..\\sounds\\raw\\shampoo\\10.mp3')
