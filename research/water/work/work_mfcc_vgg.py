12e70852a32747f9a198958f96d2b8802d1bc208
Atsuhiro <atsuhiro.fujii.98@gmail.com>
2021-10-03 23:17:57 +0900

    Update work_mfcc_vgg.py

--- research/water/work/work_mfcc_vgg.py
+++ research/water/work/work_mfcc_vgg.py
@@ -22,9 +22,9 @@ BOTTLE = 'shampoo'
 SOUND_DIR = '../sounds/temp/' + BOTTLE + '/'
 
 
-EPOCH_NUM = 3000  # 学習サイクル数
+EPOCH_NUM = 5000  # 学習サイクル数
 KERNEL_SIZE = 3  # カーネルサイズ（奇数のみ）
-BATCH_SIZE = 10000  # バッチサイズ
+BATCH_SIZE = 500  # バッチサイズ
 WINDOW_SECOND = 0.2  # 1サンプルの秒数
 STEP = 10000  # スライド幅
 TEST_ONEFILE_DATA_NUM = 1000  # 1ファイルごとのテストデータ数
