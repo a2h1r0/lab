2fde06e818614e805013c3e1495f5c99fb5f061e
Atsuhiro <atsuhiro.fujii.98@gmail.com>
2021-10-03 23:02:08 +0900

    Update work_mfcc_vgg.py

--- research/water/work/work_mfcc_vgg.py
+++ research/water/work/work_mfcc_vgg.py
@@ -155,8 +155,7 @@ def main():
     torch.manual_seed(1)
 
     # モデルの構築
-    model = models.CNN(kernel_size=KERNEL_SIZE).to(device)
-    # model = models.VGG19().to(device)
+    model = models.VGG19().to(device)
     criterion = nn.MSELoss()
     optimizer = optimizers.Adam(model.parameters(), lr=0.0002)
 
