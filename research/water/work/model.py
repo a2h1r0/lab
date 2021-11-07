a4915021c225bd3d306c50a4b7c201a5bb665c95
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-11-07 23:43:22 +0900

    Update model.py

--- research/water/work/model.py
+++ research/water/work/model.py
@@ -106,3 +106,55 @@ class DCNN(nn.Module):
         x = self.hardtanh(x)
 
         return x
+
+
+class CNN2D(nn.Module):
+    """
+    2次元CNNモデル
+    Args:
+        kernel_size (int): カーネルサイズ
+    """
+
+    def __init__(self, kernel_size=3):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=1, out_channels=8, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
+
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3)
+
+        self.conv2 = nn.Conv2d(
+            in_channels=8, out_channels=16, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
+
+        self.conv3 = nn.Conv2d(
+            in_channels=16, out_channels=64, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(64, 1)
+
+        self.hardtanh = nn.Hardtanh(min_val=0, max_val=100)
+
+    def forward(self, x):
+        """
+        Args:
+            x (:obj:`Tensor`[batch_size, 1, WINDOW_SIZE]): 音源データ
+        Returns:
+            :obj:`Tensor`[batch_size, 1]: 識別結果
+        """
+
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv3(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        x = self.hardtanh(x)
+
+        return x
