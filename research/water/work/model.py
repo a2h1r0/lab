a7ec7120b582e6e7bbf9c8f2050e51cbb44d9b44
Atsuhiro <atsuhiro.fujii.98@gmail.com>
2021-11-09 17:13:56 +0900

    Update model.py

--- research/water/work/model.py
+++ research/water/work/model.py
@@ -119,18 +119,11 @@ class CNN2D(nn.Module):
         super().__init__()
 
         self.conv1 = nn.Conv2d(
-            in_channels=1, out_channels=8, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
+            in_channels=1, out_channels=16, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
 
         self.relu = nn.ReLU()
-        self.maxpool = nn.MaxPool2d(kernel_size=3)
-
-        self.conv2 = nn.Conv2d(
-            in_channels=8, out_channels=16, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
-
-        self.conv3 = nn.Conv2d(
-            in_channels=16, out_channels=64, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
         self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Linear(64, 5)
+        self.fc = nn.Linear(16, 5)
 
         self.hardtanh = nn.Hardtanh(min_val=0, max_val=100)
 
@@ -144,13 +137,6 @@ class CNN2D(nn.Module):
 
         x = self.conv1(x)
         x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.conv2(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.conv3(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
