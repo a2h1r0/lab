fc3906e1adf6e1a94939bf433e18ea8daf9a9302
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-11-17 10:27:17 +0900

    update

---
+++ research/water/work/CrossEntropyLoss/model.py
@@ -0,0 +1,50 @@
+import torch.nn as nn
+
+
+class CNN(nn.Module):
+    """
+    CNNモデル
+
+    Args:
+        kernel_size (int): カーネルサイズ
+    """
+
+    def __init__(self, kernel_size=3):
+        super().__init__()
+
+        self.conv1 = nn.Conv1d(
+            in_channels=1, out_channels=8, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
+
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool1d(kernel_size=3)
+
+        self.conv2 = nn.Conv1d(
+            in_channels=8, out_channels=16, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
+
+        self.conv3 = nn.Conv1d(
+            in_channels=16, out_channels=64, kernel_size=kernel_size, padding=(kernel_size-1) // 2)
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(64, 2)
+
+    def forward(self, x):
+        """
+        Args:
+            x (:obj:`Tensor`[batch_size, 1, WINDOW_SIZE]): 音源データ
+        Returns:
+            :obj:`Tensor`[batch_size, 1]: 識別結果
+        """
+
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv3(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
