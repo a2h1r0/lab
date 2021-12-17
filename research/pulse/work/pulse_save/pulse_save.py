ea663379c2881c62a8fbb5540fd5a8e344acebb3
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-12-17 23:08:13 +0900

    update

--- research/pulse/work/pulse_save/pulse_save.py
+++ research/pulse/work/pulse_save/pulse_save.py
@@ -8,7 +8,7 @@ os.chdir(os.path.dirname(__file__))
 GET_TIME = 30  # 取得時間
 
 
-ser = serial.Serial('COM5', 14400)
+ser = serial.Serial('COM6', 14400)
 ser.reset_input_buffer()
 time = 0
 
@@ -17,7 +17,7 @@ filename = './' + now.strftime('%Y%m%d_%H%M%S') + '.csv'
 
 with open(filename, 'a', newline='') as f:
     writer = csv.writer(f, delimiter=',')
-    writer.writerow(['pulse'])
+    writer.writerow(['timestamp', 'pulse'])
 
     while True:
         read_data = ser.readline().rstrip().decode(encoding='utf-8', errors='ignore')
@@ -29,13 +29,13 @@ with open(filename, 'a', newline='') as f:
             if time != 0 and len(str(int(float(data[0]) / 1000000))) > len(str(int(time))) + 2:
                 continue
 
-            time = float(data[0]) / 1000000
+            time = int(data[0])
             pulse = int(data[1])
-            writer.writerow([pulse])
+            writer.writerow([time, pulse])
         else:
             continue
 
-        if time >= GET_TIME:
+        if time >= GET_TIME * 1000000:
             break
 
     ser.close()
