20ca039794f85b9d7e2e0e122e2a29d7bad738e8
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-12-25 22:45:42 +0900

    Update transfer.bat

--- research/pulse/data/PumaSmartwatch/transfer.bat
+++ research/pulse/data/PumaSmartwatch/transfer.bat
@@ -1,5 +1,5 @@
-adb connect 192.168.11.65:5555
-adb -s 192.168.11.65:5555 pull /storage/self/primary/Android/data/com.example.getpulse/files ./
-adb -s 192.168.11.65:5555 shell rm -rf /storage/self/primary/Android/data/com.example.getpulse/files
+adb connect 192.168.11.14:5555
+adb -s 192.168.11.14:5555 pull /storage/self/primary/Android/data/com.example.getpulse/files ./
+adb -s 192.168.11.14:5555 shell rm -rf /storage/self/primary/Android/data/com.example.getpulse/files
 move .\files\* .\
 rmdir .\files
