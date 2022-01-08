61319c30f6145a5323d75d2b82a93497593e00dd
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2022-01-08 23:56:55 +0900

    Update plot_voltage.py

--- research/pulse/paper/plot_voltage.py
+++ research/pulse/paper/plot_voltage.py
@@ -11,11 +11,12 @@ pwm2 = [0, 0, 2, 0, 2]
 
 plt.figure(figsize=(16, 4))
 plt.gca().get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
-plt.step(range(len(pwm1)), pwm1, color='blue')
-plt.step(range(len(pwm2)), pwm2, color='red', linestyle='dashed')
+plt.step(range(len(pwm1)), pwm1, label='Electrode 1', color='blue')
+plt.step(range(len(pwm2)), pwm2, label='Electrode 2', color='red', linestyle='dashed')
 plt.xlabel('Index', fontsize=26)
 plt.ylabel('Voltage [V]', fontsize=26)
 plt.tick_params(labelsize=26)
+plt.legend(fontsize=26, loc='upper right', bbox_to_anchor=(1, -0.1))
 
 plt.savefig('../figures/voltage_wave.svg', bbox_inches='tight', pad_inches=0)
 
