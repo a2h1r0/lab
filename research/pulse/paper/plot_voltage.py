c8d981c382e305a1dbcbece636a92ab38ab526dd
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2022-01-08 23:46:51 +0900

    Create plot_voltage.py

---
+++ research/pulse/paper/plot_voltage.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import os
+os.chdir(os.path.dirname(__file__))
+
+
+pwm1 = [2, 2, 0, 2, 0]
+pwm2 = [0, 0, 2, 0, 2]
+
+
+plt.figure(figsize=(16, 4))
+plt.gca().get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
+plt.step(range(len(pwm1)), pwm1, color='blue')
+plt.step(range(len(pwm2)), pwm2, color='red', linestyle='dashed')
+plt.xlabel('Index', fontsize=26)
+plt.ylabel('Voltage [V]', fontsize=26)
+plt.tick_params(labelsize=26)
+
+plt.savefig('../figures/voltage_wave.svg', bbox_inches='tight', pad_inches=0)
+
+plt.show()
