325b5946584dedc36e6583d846da582a1a23c977
Atsuhiro <53073064+a2h1r0@users.noreply.github.com>
2024-02-28 23:08:55 +0900

    Update make_data.py

--- research/eye/make_data.py
+++ research/eye/make_data.py
@@ -20,121 +20,109 @@ WINDOW_SIZE = 30  # ウィンドウサイズ（秒）
 STEP = 3  # ステップ幅（秒）
 
 
-def preprocess():
+def preprocess(filename):
     """
     データの前処理
 
+    Args:
+        filename (string): ファイル名
     Returns:
         list: 前処理データ
-        list: ラベル
     """
 
-    train_data, train_labels = [], []
-    files = glob.glob(f'{RAW_DIR}/**/*.csv', recursive=True)
+    def make_label(string):
+        """
+        ラベルの生成
 
-    for filename in files:
-        # with open(filename) as f:
-        #     reader = csv.reader(f)
-        #     next(reader)
-        #     # データファイルの最終行が空行
-        #     raw = [row[1:8] for row in reader][:-1]
+        Args:
+            string (string): 識別情報
+        Returns:
+            string: ラベル
+        """
 
-        # data = slide_window(raw, make_label(filename))
+        label = ''
 
-        save_file = filename.replace(RAW_DIR, SAVE_DIR)
-        if not os.path.exists(os.path.dirname(save_file)):
-            os.makedirs(os.path.dirname(save_file))
+        if 'drunk' in string:
+            label = 'drunk'
+        else:
+            label = 'sober'
 
-        with open(save_file, 'w', newline='') as f:
-            result_writer = csv.writer(f)
-            result_writer.writerow(['start_index', 'eyeMoveUp', 'eyeMoveDown',
-                                   'eyeMoveLeft', 'eyeMoveRight', 'blinkSpeed', 'blinkStrength', 'label'])
+        return label
 
-            # 書き込み処理実装
-            # start_indexより，ローデータのid使ってstart_idのほうが良さそう
-            # 書き込み処理はmainで行う？
+    def slide_window(raw, label):
+        """
+        スライディングウィンドウ
 
-            # # 結果の保存
-            # for filename, answer, prediction in zip(test_files, answers, predictions):
-            #     result_writer.writerow(
-            #         [filename.split('\\')[-1], answer, prediction])
-            # result_writer.writerow(
-            #     ['(Accuracy)', accuracy_score(answers, predictions)])
+        Args:
+            raw (list): ローデータ
+            label (list): ラベル
+        Returns:
+            list: スライディングウィンドウ後のデータ
+        """
 
+        data = []
 
-def make_label(string):
-    """
-    ラベルの生成
+        # todo: start_indexの増分変更
+        for start_index, start in enumerate(raw):
+            start_id = raw[start_index][0]
+            start_time = datetime.datetime.fromisoformat(start[1])
+            end_time = start_time + datetime.timedelta(seconds=WINDOW_SIZE)
 
-    Args:
-        string (string): 識別情報
-    Returns:
-        string: ラベル
-    """
+            window = []
+            for index, row in enumerate(raw[start_index:]):
+                time = datetime.datetime.fromisoformat(row[1])
+                if time > end_time:
+                    break
+                window.append(
+                    [start_id, *list(map(lambda value: float(value), row[2:])), label])
+                end_index = start_index + index
 
-    label = ''
+            data.extend(window)
 
-    if 'drunk' in string:
-        label = 'drunk'
-    else:
-        label = 'sober'
-
-    return label
-
-
-def slide_window(raw, label):
-    """
-    スライディングウィンドウの実行
-
-    Args:
-        raw (list): ローデータ
-        label (list): ラベル
-    Returns:
-        list: スライディングウィンドウ後のデータ
-    """
-
-    data = []
-
-    for start_index, start in enumerate(raw):
-        start_time = datetime.datetime.fromisoformat(start[0])
-        end_time = start_time + datetime.timedelta(seconds=WINDOW_SIZE)
-
-        window = []
-        for index, row in enumerate(raw[start_index:]):
-            time = datetime.datetime.fromisoformat(row[0])
-            if time > end_time:
+            # 末尾到達
+            if end_index == len(raw) - 1:
                 break
-            window.append(
-                [start_index, *list(map(lambda value: float(value), row[1:])), label])
-            end_index = start_index + index
 
-        data.extend(window)
+        return data
 
-        # 末尾到達
-        if end_index == len(raw) - 1:
-            break
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader)
+        # データファイルの最終行が空行
+        raw = [row[0:8] for row in reader][:-1]
+
+    data = slide_window(raw, make_label(filename))
 
     return data
 
 
+def save_data(filename, data):
+    """
+    データの保存
+
+    Args:
+        filename (string): 保存ファイル名
+        data (list): データ
+    """
+
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['start_id', 'eyeMoveUp', 'eyeMoveDown', 'eyeMoveLeft',
+                        'eyeMoveRight', 'blinkSpeed', 'blinkStrength', 'label'])
+        writer.writerows(data)
+
+
 def main():
-    train_data, train_labels = preprocess()
+    files = glob.glob(f'{RAW_DIR}/**/*.csv', recursive=True)
 
-    now = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
-    result_file = './result/{}.csv'.format(now)
-    with open(result_file, 'w', newline='') as f:
-        result_writer = csv.writer(f)
-        result_writer.writerow(['TestFile', 'Answer', 'Prediction'])
+    for filename in files:
+        data = preprocess(filename)
 
-        loss_all = train()
-        predictions, answers, test_files = test()
-
-        # 結果の保存
-        for filename, answer, prediction in zip(test_files, answers, predictions):
-            result_writer.writerow(
-                [filename.split('\\')[-1], answer, prediction])
-        result_writer.writerow(
-            ['(Accuracy)', accuracy_score(answers, predictions)])
+        save_file = filename.replace(RAW_DIR, SAVE_DIR)
+        save_data(save_file, data)
 
 
 if __name__ == '__main__':
