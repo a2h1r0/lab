4399c5f4de56e973e28fc46745400ba57249e0a1
Atsuhiro <53073064+a2h1r0@users.noreply.github.com>
2024-04-12 11:38:34 +0900

    archive jins project

---
+++ research/eye/jins/make_data.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+import math
+import csv
+import glob
+import re
+import datetime
+import random
+import copy
+import sys
+import os
+os.chdir(os.path.dirname(__file__))
+
+
+WINDOW_SIZE = 10  # ウィンドウサイズ（秒）
+STEP = 3  # ステップ幅（秒）
+
+
+RAW_DIR = './data/raw'
+SAVE_DIR = f'./data/preprocess/window_{WINDOW_SIZE}'
+
+
+def preprocess(filename):
+    """
+    データの前処理
+
+    Args:
+        filename (string): ファイル名
+    Returns:
+        list: 前処理データ
+    """
+
+    def make_label(string):
+        """
+        ラベルの生成
+
+        Args:
+            string (string): 識別情報
+        Returns:
+            string: ラベル
+        """
+
+        label = ''
+
+        if 'drunk' in string:
+            label = 'drunk'
+        else:
+            label = 'sober'
+
+        return label
+
+    def slide_window(raw, label):
+        """
+        スライディングウィンドウ
+
+        Args:
+            raw (list): ローデータ
+            label (list): ラベル
+        Returns:
+            list: スライディングウィンドウ後のデータ
+        """
+
+        data = []
+
+        next_start_time = None
+        for start_index, start in enumerate(raw):
+            start_time = datetime.datetime.fromisoformat(start[1])
+            if next_start_time and start_time < next_start_time:
+                continue
+
+            start_id = raw[start_index][0]
+            end_time = start_time + datetime.timedelta(seconds=WINDOW_SIZE)
+            next_start_time = start_time + datetime.timedelta(seconds=STEP)
+
+            window = []
+            for index, row in enumerate(raw[start_index:]):
+                time = datetime.datetime.fromisoformat(row[1])
+                if time > end_time:
+                    break
+                window.append(
+                    [start_id, label, *list(map(lambda value: float(value), row[2:]))])
+                end_index = start_index + index
+
+            data.extend(window)
+
+            # 末尾到達
+            if end_index == len(raw) - 1:
+                break
+
+        return data
+
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader)
+        # データファイルの最終行が空行
+        raw = [row[0:8] for row in reader][:-1]
+
+    data = slide_window(raw, make_label(filename))
+
+    return data
+
+
+def save_data(filename, data):
+    """
+    データの保存
+
+    Args:
+        filename (string): 保存ファイル名
+        data (list): データ
+    """
+
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['start_id', 'label', 'eyeMoveUp', 'eyeMoveDown',
+                        'eyeMoveLeft', 'eyeMoveRight', 'blinkSpeed', 'blinkStrength'])
+        writer.writerows(data)
+
+
+def main():
+    files = glob.glob(
+        f'{RAW_DIR}/**/*_C152633DCDAE_currentData_m1.csv', recursive=True)
+
+    for filename in files:
+        data = preprocess(filename)
+
+        save_file = filename.replace(RAW_DIR, SAVE_DIR)
+        save_data(save_file, data)
+
+
+if __name__ == '__main__':
+    main()
