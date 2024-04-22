8eec2fe48e35a2404c03efba9924f2f0a0011248
Atsuhiro <53073064+a2h1r0@users.noreply.github.com>
2024-04-22 23:00:19 +0900

    Update make_data.py

--- research/eye/make_data.py
+++ research/eye/make_data.py
@@ -14,6 +14,9 @@ os.chdir(os.path.dirname(__file__))
 
 WINDOW_SIZE = 10  # ウィンドウサイズ（秒）
 STEP = 3  # ステップ幅（秒）
+USE_COLUMNS = [
+    'Recording timestamp', 'Recording date', 'Gaze point X', 'Gaze point Y', 'Gaze point left X', 'Gaze point left Y', 'Gaze point right X', 'Gaze point right Y', 'Gaze direction left X', 'Gaze direction left Y', 'Gaze direction left Z', 'Gaze direction right X', 'Gaze direction right Y', 'Gaze direction right Z', 'Pupil diameter left', 'Pupil diameter right'
+]   # 使用カラム
 
 
 RAW_DIR = './data/raw'
@@ -63,23 +66,26 @@ def preprocess(filename):
         data = []
 
         next_start_time = None
-        for start_index, start in enumerate(raw):
-            start_time = datetime.datetime.fromisoformat(start[1])
+        for start_index, start in raw.iterrows():
+            start_time = start['Recording timestamp']
             if next_start_time and start_time < next_start_time:
                 continue
 
-            start_id = raw[start_index][0]
-            end_time = start_time + datetime.timedelta(seconds=WINDOW_SIZE)
-            next_start_time = start_time + datetime.timedelta(seconds=STEP)
+            end_time = start_time + WINDOW_SIZE * 1000000
+            next_start_time = start_time + STEP * 1000000
 
-            window = []
-            for index, row in enumerate(raw[start_index:]):
-                time = datetime.datetime.fromisoformat(row[1])
-                if time > end_time:
-                    break
-                window.append(
-                    [start_id, label, *list(map(lambda value: float(value), row[2:]))])
-                end_index = start_index + index
+            window = raw[(start_time <= raw['Recording timestamp'])
+                         & (raw['Recording timestamp'] < end_time)]
+            # ここまでOK
+
+            # window = []
+            # for index, row in enumerate(raw[start_index:]):
+            #     time = datetime.datetime.fromisoformat(row[1])
+            #     if time > end_time:
+            #         break
+            window.append(
+                [start_time, label, *list(map(lambda value: float(value), row[2:]))])
+            # end_index = start_index + index
 
             data.extend(window)
 
@@ -89,12 +95,7 @@ def preprocess(filename):
 
         return data
 
-    with open(filename) as f:
-        reader = csv.reader(f)
-        next(reader)
-        # データファイルの最終行が空行
-        raw = [row[0:8] for row in reader][:-1]
-
+    raw = pd.read_csv(filename, sep='\t', usecols=USE_COLUMNS)
     data = slide_window(raw, make_label(filename))
 
     return data
@@ -121,7 +122,7 @@ def save_data(filename, data):
 
 def main():
     files = glob.glob(
-        f'{RAW_DIR}/**/*_C152633DCDAE_currentData_m1.csv', recursive=True)
+        f'{RAW_DIR}/**/*.tsv', recursive=True)
 
     for filename in files:
         data = preprocess(filename)
