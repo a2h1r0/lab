8fb01e151191447a6b233cf1e0727ea60b96ba56
Atsuhiro <53073064+a2h1r0@users.noreply.github.com>
2024-06-10 13:27:50 +0900

    Update get_data.py

--- research/eye/tobii/get_data.py
+++ research/eye/tobii/get_data.py
@@ -1,12 +1,16 @@
-import numpy as np
+import pandas as pd
 import tobii_research as tr
 import time
 import csv
 import os
+import datetime
 
 
+SUBJECT = 'fujii'
 COLLECT_TIME = 5
-SAVE_DIR = f'../data'
+
+
+SAVE_DIR = f'../data/{SUBJECT}'
 
 
 def connect_eyetracker():
@@ -21,16 +25,28 @@ def connect_eyetracker():
     return tr.find_all_eyetrackers()[0]
 
 
-def check_not_nan(data):
-    return np.count_nonzero(np.isnan(data)) == 0
-
-
 def get_gaze_data(gaze_data):
-    if check_not_nan(gaze_data['left_gaze_point_on_display_area']) and check_not_nan(gaze_data['right_gaze_point_on_display_area']):
-        collection_data.append(
-            gaze_data['left_gaze_point_on_display_area'] + gaze_data['right_gaze_point_on_display_area'])
-        print(
-            f'Left eye: ({gaze_data["left_gaze_point_on_display_area"]}) \t Right eye: ({gaze_data["right_gaze_point_on_display_area"]})')
+    """
+    データの取得
+
+    Args:
+        gaze_data (dict): 取得データ
+    """
+
+    keys, values = [], []
+    for (key, value) in list(gaze_data.items()):
+        if isinstance(value, tuple):
+            keys.extend([f'{key}_{i + 1}' for i in range(len(value))])
+            values.extend(list(value))
+        else:
+            keys.append(key)
+            values.append(value)
+
+    if collection_data == []:
+        collection_data.append(keys)
+    collection_data.append(values)
+
+    print(f'Timestamp: {gaze_data["device_time_stamp"]}')
 
 
 def save_data(save_dir):
@@ -41,15 +57,16 @@ def save_data(save_dir):
         save_dir (string): 保存ディレクトリ名
     """
 
+    # 保存ディレクトリなんとかする
+    print(os.path.exists(os.path.dirname(save_dir)))
     if not os.path.exists(os.path.dirname(save_dir)):
         os.makedirs(os.path.dirname(save_dir))
 
-    filename = f'{save_dir}/test.csv'
+    now = datetime.datetime.today()
+    filename = f'{save_dir}/{now.strftime("%Y%m%d_%H%M%S")}.csv'
 
     with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(['left_gaze_point_on_display_area_x', 'left_gaze_point_on_display_area_y',
-                        'right_gaze_point_on_display_area_x', 'right_gaze_point_on_display_area_y'])
         writer.writerows(collection_data)
 
 
