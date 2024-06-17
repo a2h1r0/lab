f6952e7cc9deb0c3a7e0e2890dc6d67975a6a193
Atsuhiro <53073064+a2h1r0@users.noreply.github.com>
2024-06-17 22:47:40 +0900

    Create preprocess.py

---
+++ research/eye/preprocess.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import math
+import csv
+import glob
+import re
+import datetime
+import random
+import copy
+import sys
+import os
+os.chdir(os.path.dirname(__file__))
+
+
+FILE_WINDOW_SIZE = 20  # ウィンドウサイズ（秒）
+WINDOW_SIZE = 0.1  # ウィンドウサイズ（秒）
+STEP = 0.05  # ステップ幅（秒）
+
+
+RAW_DIR = f'./data/split/window_{FILE_WINDOW_SIZE}'
+SAVE_DIR = f'./data/preprocess/window_{WINDOW_SIZE}'
+
+
+def preprocess(filename):
+    """
+    データの前処理
+
+    Args:
+        filename (string): ファイル名
+    Returns:
+        list: 前処理データ
+    """
+
+    def slide_window(raw):
+        """
+        スライディングウィンドウ
+
+        Args:
+            raw (list): ローデータ
+        Returns:
+            list: スライディングウィンドウ後のデータ
+        """
+
+        feature = [raw.columns.tolist()]
+
+        next_start_time = None
+        for start_index, start in raw.iterrows():
+            start_time = start['Recording timestamp']
+            if next_start_time and start_time < next_start_time:
+                continue
+
+            end_time = start_time + WINDOW_SIZE * 1000000
+            next_start_time = start_time + STEP * 1000000
+
+            window = raw[(start_time <= raw['Recording timestamp'])
+                         & (raw['Recording timestamp'] < end_time)]
+            feature.append(window.iloc[0, :2].values.tolist(
+            ) + window.iloc[:, 2:].mean().values.tolist())
+
+            # 末尾到達
+            if window.iloc[-1].name == len(raw.index) - 1:
+                break
+
+        return feature
+
+    raw = pd.read_csv(filename, header=0)
+    feature = slide_window(raw)
+
+    return feature
+
+
+def save_data(save_file, feature):
+    """
+    データの保存
+
+    Args:
+        save_file (string): 保存ファイル名
+        feature (list): 特徴量データ
+    """
+
+    if not os.path.exists(os.path.dirname(save_file)):
+        os.makedirs(os.path.dirname(save_file))
+
+    with open(save_file, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(feature)
+
+
+def main():
+    files = glob.glob(
+        f'{RAW_DIR}/**/*.csv', recursive=True)
+
+    for filename in files:
+        feature = preprocess(filename)
+
+        save_file = filename.replace(RAW_DIR, SAVE_DIR)
+        save_data(save_file, feature)
+
+
+if __name__ == '__main__':
+    main()
