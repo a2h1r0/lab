a94f102153256dcb2bfe2ae7f46dfe67d17942c7
Atsuhiro <53073064+a2h1r0@users.noreply.github.com>
2024-06-17 23:46:46 +0900

    Update main.py

--- research/eye/main.py
+++ research/eye/main.py
@@ -19,7 +19,7 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 
-DATA_DIR = './data/preprocess/window_0.5/'
+DATA_DIR = './data/preprocess/dropna/'
 TRAIN_SUBJECTS = ['1']
 TEST_SUBJECTS = ['2']
 
